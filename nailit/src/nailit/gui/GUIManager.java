eb5a22fe01b3bff992019d4ac1d55f2d12d93715
Naomi Leow <naomileowwx@gmail.com>
2013-09-21 16:53:54 +0800

    with basic implementation of command bar with keyboard action listener so pressing of 'enter' key triggers action by GUIManager

--- nailit/src/nailit/gui/GUIManager.java
+++ nailit/src/nailit/gui/GUIManager.java
@@ -9,11 +9,15 @@ import java.awt.BorderLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
 
 public class GUIManager {
 
 	private JFrame frame;
 	private static String APPLICATION_NAME = "NailIt!";
+	private CommandBar commandBar;
 	
 	private final WindowListener windowClosePressed =  new WindowAdapter(){
 		public void windowClosing(WindowEvent event){
@@ -41,6 +45,8 @@ public class GUIManager {
 	 */
 	public GUIManager() {
 		initialize();
+		//stub to be modified later
+		
 	}
 	
 	private void addListenersToMainFrame(){
@@ -50,10 +56,14 @@ public class GUIManager {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		commandBar = new CommandBar(this);
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 300); //temp to be replaced by constants later
 		frame.setTitle(APPLICATION_NAME);
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+		frame.getContentPane().add(commandBar,BorderLayout.SOUTH);
+		
 		addListenersToMainFrame();
 	}
 	
@@ -62,7 +72,8 @@ public class GUIManager {
 	 * @param input
 	 */
 	protected void executeUserInputCommand(String input){
-		
+		System.out.println(input);
+		commandBar.clearUserInput();
 	}
 
 }
