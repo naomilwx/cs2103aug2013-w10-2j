6ea6b9f1a01ae2700880dc16085871a827bf7747
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-09-24 15:27:01 +0800

    change the ParserManager class

--- nailit/src/nailit/logic/parser/AddParser.java
+++ nailit/src/nailit/logic/parser/AddParser.java
@@ -1,9 +1,13 @@
 package nailit.logic.parser;
 
+import nailit.logic.ParserResult;
+
 public class AddParser extends ParserManager {
 
 	public void parser()
 	{
 		
 	}
+	
+	public ParserResult execute(){};
 }
