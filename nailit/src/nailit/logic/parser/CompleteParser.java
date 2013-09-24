1865fea9ca1342eacf828aedf2ac927fff834dc3
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-09-24 16:03:33 +0800

    add the constructor for each children class in parser

--- nailit/src/nailit/logic/parser/CompleteParser.java
+++ nailit/src/nailit/logic/parser/CompleteParser.java
@@ -4,6 +4,13 @@ import nailit.logic.ParserResult;
 
 public class CompleteParser extends ParserManager{
 
+private String userCommand;
+	
+	public CompleteParser (String command)
+	{
+		userCommand = command;
+	}
+	
 	public ParserResult execute()
 	{
 		ParserResult resultExecution = new ParserResult();
