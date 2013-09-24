6d38ccf476ea1e2cd6c4b4f048cebb05e50c8c06
Naomi Leow <naomileowwx@gmail.com>
2013-09-24 13:16:46 +0800

    merged changes from remote

--- nailit/src/nailit/storage/StorageManager.java
+++ nailit/src/nailit/storage/StorageManager.java
@@ -1,11 +1,19 @@
 package nailit.storage;
 
 public class StorageManager {
+	private DataManager inMemory;
+	private FileManager hardDisk;
+	
+	StorageManager(){
+		inMemory = new DataManager();
+		hardDisk = new FileManager();
+	}
+	
 	public int add(){
 		return 0;
 	}
 	//update stub
-	public boolean update(){
+	public boolean retrieve(){
 		return true;
 	}
 	public boolean remove(){
