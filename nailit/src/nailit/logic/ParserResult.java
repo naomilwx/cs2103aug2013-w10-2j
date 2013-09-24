f652791e03911371760c3f4e2baac0431b5af272
Naomi Leow <naomileowwx@gmail.com>
2013-09-24 13:15:20 +0800

    formatting of events printout half done

---
+++ nailit/src/nailit/logic/ParserResult.java
@@ -0,0 +1,16 @@
+package nailit.logic;
+
+import java.util.*;
+
+public class ParserResult
+{
+	ArrayList<String> command = new ArrayList<String> ();
+	
+	private static String Name;
+	private static Date StartTime;
+	private static Date EndTime;
+	private static Integer Priority;
+	private static String Tag;
+	
+	
+}
---
+++ nailit/src/nailit/logic/command/Command.java
@@ -0,0 +1,20 @@
+package nailit.logic.command;
+import nailit.logic.*;
+
+public class Command {
+	
+	public static ParserResult ParserResultInstance;
+	
+	public Command (ParserResult ResultInstance) // constructor
+	{
+		ParserResultInstance = ResultInstance;
+	}
+	
+	public Result executeCommand()
+	{
+		Result executeResult = new Result();
+		// add the method
+		return executeResult;
+	}
+	
+}
