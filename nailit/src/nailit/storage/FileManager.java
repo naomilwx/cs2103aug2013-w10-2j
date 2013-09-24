fd2dfadd8b10033fb2d986834c6bc37955694114
Wei Wenbo <weiwenbo3@yahoo.com.sg>
2013-09-24 16:26:10 +0800

    pull from remote

--- nailit/src/nailit/storage/FileManager.java
+++ nailit/src/nailit/storage/FileManager.java
@@ -1,6 +1,9 @@
 package nailit.storage;
 
 public class FileManager {
+	public int add(int ID, String name,String startDate,String endDate,int priority,String tag,String desc){
+		return -2;
+	}
 	public FileManager(){
 		
 	}
