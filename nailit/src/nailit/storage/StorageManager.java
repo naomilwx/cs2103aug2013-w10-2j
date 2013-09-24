e4a6d1900ccda51a6adb903cc83ddc744be1834e
Shuzhi <nusszyu@gmail.com>
2013-09-24 12:42:51 +0800

    some changes on class command

---
+++ nailit/src/nailit/storage/StorageManager.java
@@ -0,0 +1,14 @@
+package nailit.storage;
+
+public class StorageManager {
+	public int add(){
+		return 0;
+	}
+	//update stub
+	public boolean update(){
+		return true;
+	}
+	public boolean remove(){
+		return true;
+	}
+}
