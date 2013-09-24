77345b52ac1ecddb752a6759168601a83f5f4b27
Wei Wenbo <weiwenbo3@yahoo.com.sg>
2013-09-24 16:02:42 +0800

    reconfigure build path

--- nailit/src/nailit/storage/DataManager.java
+++ nailit/src/nailit/storage/DataManager.java
@@ -1,6 +1,22 @@
 package nailit.storage;
 
 public class DataManager {
+	private final int TASKID_NULL = -1;
+	public int add(int ID, String name,String startDate,String endDate,int priority,String tag,String desc){
+		if(addedBefore(ID)){
+			
+		}
+		else{
+			ID = generateNewID();
+		}
+	}
+	public boolean addedBefore(int ID){
+		return ID != TASKID_NULL;
+	}
+	public int generateNewID(){
+		return -1;
+	}
+	//Constructor
 	public DataManager(){
 		
 	}
