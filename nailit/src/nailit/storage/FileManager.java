9e546c436c46f4000aebaa28e1cc1e42d04fc5d1
Wei Wenbo <weiwenbo3@yahoo.com.sg>
2013-09-24 21:27:42 +0800

    try to push without password

--- nailit/src/nailit/storage/FileManager.java
+++ nailit/src/nailit/storage/FileManager.java
@@ -1,10 +1,24 @@
 package nailit.storage;
+import java.util.ArrayList;
+
+import nailit.Task;
+
+import org.joda.time.DateTime;
 
 public class FileManager {
-	public int add(int ID, String name,String startDate,String endDate,int priority,String tag,String desc){
-		return -2;
-	}
-	public FileManager(){
+	
+	//Constructor
+	public FileManager(String path){
 		
 	}
+	public int add(int ID, String name,DateTime startDate,DateTime endDate,int priority,String tag,String desc){
+		return -2;
+	}
+	public void read(ArrayList<Task> tasks){
+		
+	}
+	public void save(ArrayList<Task> tasks){
+		
+	}
+	
 }
