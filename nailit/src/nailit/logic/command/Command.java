c6ff188c6260ab38a20a277d542806b554759bbf
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-09-24 13:12:56 +0800

    merged

--- nailit/src/nailit/logic/command/Command.java
+++ nailit/src/nailit/logic/command/Command.java
@@ -1,5 +1,6 @@
 package nailit.logic.command;
 import nailit.logic.*;
+import nailit.storage.StorageManager;
 
 public class Command {
 	
@@ -12,8 +13,8 @@ public class Command {
 	
 	public CommandResult executeCommand()
 	{
-		CommandResult executeResult = new CommandResult();
-		// add the method
+		Result executeResult = new Result();
+		StorageManagerStub storer = new StorageManagerStub();
 		
 		return executeResult;
 	}
