077f5bda12b5a36122e5891069f683cd6cd956e2
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-09-24 13:06:52 +0800

    add a few subclass for parserManager

---
+++ nailit/src/nailit/logic/parser/CompleteParser.java
@@ -0,0 +1,13 @@
+package nailit.logic.parser;
+
+public class CompleteParser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
