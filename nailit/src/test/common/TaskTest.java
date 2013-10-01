f4bd95ca3481e6ae73aed4f22b864d8e86b7868a
Naomi Leow <naomileowwx@gmail.com>
2013-10-02 00:36:43 +0800

    added empty task test

---
+++ nailit/src/test/common/TaskTest.java
@@ -0,0 +1,14 @@
+package test.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
