5108e227970c1d2a0eeb135ec779754834d80047
Naomi Leow <naomileowwx@gmail.com>
2013-10-03 21:49:33 +0800

    added listener to set focus on display

--- nailit/src/nailit/gui/DisplayArea.java
+++ nailit/src/nailit/gui/DisplayArea.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
@@ -15,6 +16,8 @@ public class DisplayArea extends JPanel {
 	private static final int WINDOW_BOTTOM_BUFFER = GUIManager.WINDOW_BOTTOM_BUFFER;
 	
 	private GUIManager GUIBoss;
+	private JScrollPane scrollPane;
+	
 	private int displayWidth;
 	private int displayHeight;
 	/**
@@ -23,6 +26,7 @@ public class DisplayArea extends JPanel {
 	public DisplayArea(final GUIManager GUIMain, int containerWidth, int containerHeight) {
 		GUIBoss = GUIMain;
 		configureDisplayArea(containerWidth, containerHeight);
+		createAndAddScrollPane();
 	}
 	private void configureDisplayArea(int containerWidth, int containerHeight){
 		displayWidth = containerWidth - X_BUFFER_WIDTH - WINDOW_RIGHT_BUFFER;
@@ -32,5 +36,12 @@ public class DisplayArea extends JPanel {
 		this.setBackground(DISPLAYAREA_BACKGROUND_COLOR);
 		this.setLocation(X_BUFFER_WIDTH, Y_BUFFER_HEIGHT);
 		this.setSize(displayWidth, displayHeight);
+	}
+	private void createAndAddScrollPane(){
+		scrollPane = new JScrollPane();
+		add(scrollPane);
+	}
+	protected void setFocus(){
+		scrollPane.requestFocus();
 	}
 }
