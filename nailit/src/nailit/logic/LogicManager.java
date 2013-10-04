e549ba398eaeffec9250c1a52f4457bdc8ed4934
Shuzhi <nusszyu@gmail.com>
2013-10-04 16:58:31 +0800

    change the structure of the CommandManager part in ManagerLogic, add Command class as the parent classes

--- nailit/src/nailit/logic/LogicManager.java
+++ nailit/src/nailit/logic/LogicManager.java
@@ -8,16 +8,30 @@ import nailit.logic.parser.*;
 
 public class LogicManager{
 	
-	private ParserResult ParserResultInstance;
+//	private ParserResult ParserResultInstance;
 	private Parser ParserInstance;
-	private Command CommandInstance;
+	private CommandManager commandInstance;
 	
+	// constructor
+	public LogicManager() {
+//		ParserInstance = new Parser();
+		commandInstance = new CommandManager();
+	}
 	public Result executeCommand(String OriginalCommand){
 		Result executeCommandResult = new Result();
+		
+		// Shuzhi: commands for Shuzhi and Binbin
+		// my thought of the work flow is this: we only have one 
+		// instance for ParserManager and CommandManager, which act 
+		// like two proxies. And everytime there is a Original command,
+		// the only instance manager will handle it, and then pass the
+		// parser result to the only instance of CommmandManager.
+		// For the history of done operations, we store it in the only 
+		// instance of the CommandManager.
+		// This is why we need a constructor
 		ParserInstance = new Parser(OriginalCommand);
-		ParserResultInstance = ParserInstance.execute();
-		CommandInstance = new Command(ParserResultInstance);
-		executeCommandResult = CommandInstance.executeCommand();
+		ParserResult parserResultInstance = ParserInstance.execute();
+		executeCommandResult = commandInstance.executeCommand(parserResultInstance);
 		return  executeCommandResult;
 	}
 	
--- nailit/src/nailit/logic/command/CommandAdd.java
+++ nailit/src/nailit/logic/command/CommandAdd.java
@@ -2,13 +2,14 @@ package nailit.logic.command;
 
 import nailit.common.Result;
 import nailit.logic.ParserResult;
+import nailit.storage.StorageManager;
 
 public class CommandAdd extends Command{
 	private Result executedResult;
 	
 	// constructor
-	public CommandAdd(ParserResult ResultInstance) {
-		super(ResultInstance);
+	public CommandAdd(ParserResult ResultInstance, StorageManager storerToUse) {
+		super(ResultInstance, storerToUse);
 	}
 	
 	public Result execute() {
