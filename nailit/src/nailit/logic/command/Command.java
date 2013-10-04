a9a6403797011d7c1a292797e65c21cf11767d92
Shuzhi <nusszyu@gmail.com>
2013-10-04 16:58:31 +0800

    change the structure of the CommandManager part in ManagerLogic, add Command class as the parent classes

--- nailit/src/nailit/logic/command/Command.java
+++ nailit/src/nailit/logic/command/Command.java
@@ -1,87 +1,20 @@
 package nailit.logic.command;
 
 import nailit.common.Result;
-import nailit.logic.*;
-
-import nailit.storage.StorageManager;
-import nailit.logic.CommandType;
 import nailit.logic.ParserResult;
-
+import nailit.storage.StorageManager;
 
 public class Command {
-	
+
 	private static ParserResult parserResultInstance;
+	
 	private static StorageManager storer;
+	
 	private static Result executedResult;
-	
-	public Command (ParserResult ResultInstance) // constructor
-	{
-		parserResultInstance = ResultInstance;
-		storer = new StorageManager();
-		executedResult = new Result();
+	// constructor
+	public Command(ParserResult resultInstance, StorageManager storerToUse) {
+		parserResultInstance =  resultInstance;
+		this.storer = storerToUse;
 	}
 	
-	public Result executeCommand()
-	{
-		doExecution();
-		return executedResult;
-	}
-
-	private void doExecution() {
-		CommandType commandType = parserResultInstance.getCommand();
-		switch(commandType) {
-			case ADD: {
-				executedResult = add();
-				break;
-			}
-			case COMPLETE: {
-				executedResult = complete();
-				break;
-			} 
-			case DELETE: {
-				executedResult = delete();
-				break;
-			}
-			case SEARCH: {
-				executedResult = search();
-				break;
-			}
-			case UPDATE: {
-				executedResult = update();
-				break;
-			}
-			case INVALID: {
-				break;
-			}
-			default: {
-				
-			}
-		}
-	}
-
-	private Result update() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private Result search() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private Result delete() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private Result complete() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private Result add() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
--- nailit/src/nailit/logic/command/CommandManager.java
+++ nailit/src/nailit/logic/command/CommandManager.java
@@ -1,5 +1,96 @@
 package nailit.logic.command;
 
+import java.util.Vector;
+
+import nailit.common.Result;
+import nailit.logic.*;
+
+import nailit.storage.StorageManager;
+import nailit.logic.CommandType;
+import nailit.logic.ParserResult;
+
+
 public class CommandManager {
+	
+	private StorageManager storer;
+	
+	private Result executedResult;
+	
+	private ParserResult parserResultInstance;
+	
+	// the vector object is used to store the done operations
+	private Vector<Command> operationsHistory;
+	
+	public CommandManager () // constructor
+	{
+//		parserResultInstance = resultInstance;
+		storer = new StorageManager();
+		executedResult = new Result();
+		operationsHistory = new Vector<Command>();
+	}
+	
+	public Result executeCommand(ParserResult parserResultInstance)
+	{
+		this.parserResultInstance = parserResultInstance;
+		doExecution();
+		return executedResult;
+	}
+
+	private void doExecution() {
+		CommandType commandType = parserResultInstance.getCommand();
+		switch(commandType) {
+			case ADD: {
+				executedResult = add();
+				break;
+			}
+			case COMPLETE: {
+				executedResult = complete();
+				break;
+			} 
+			case DELETE: {
+				executedResult = delete();
+				break;
+			}
+			case SEARCH: {
+				executedResult = search();
+				break;
+			}
+			case UPDATE: {
+				executedResult = update();
+				break;
+			}
+			case INVALID: {
+				break;
+			}
+			default: {
+				
+			}
+		}
+	}
+
+	private Result update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Result search() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Result delete() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Result complete() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Result add() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
