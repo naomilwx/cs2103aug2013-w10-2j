b8feda3b87b546cf755239b2c051afc509cd182e
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-10-05 12:23:38 +0800

    attribute should not be the static

---
+++ nailit/src/nailit/storage/DataManager.java
@@ -0,0 +1,69 @@
+package nailit.storage;
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+
+import nailit.common.TASK_PRIORITY;
+import nailit.common.Task;
+
+public class DataManager {
+	
+	/**
+	 * Private Fields
+	 * */
+	private final int TASKID_NULL = -1;
+	private ArrayList<String> dataList = new ArrayList<String>();
+	
+	/**
+	 * Constructor
+	 * */
+	public DataManager(){
+	
+	}
+
+	/**
+	 * Public Methods
+	 * */
+	public int add(int ID, String taskString){
+		
+		if(!addedBefore(ID)){
+			ID = generateNewID();
+			dataList.add(taskString);
+		}
+		else{
+			dataList.set(ID, taskString);
+		}
+		return ID;
+	}
+	
+	public String retrieve(int lineNum){
+		return null;
+	}
+	
+	public String remove(int lineNum){
+		return null;
+	}
+	
+	public ArrayList<String> getDataList(){
+		return null;
+	}
+	
+	public void setDataList(ArrayList<String> d){
+		
+	}
+	
+	public DataManager clone(){
+		return null;
+	}
+	
+	/**
+	 * Private Methods
+	 * */
+	private int generateNewID(){
+		return -1;
+	}
+	
+	private boolean addedBefore(int ID){
+		return ID != TASKID_NULL;
+	}
+}
