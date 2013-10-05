87fa923047e1e059fdf2b82c88de85a1c8325f13
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-10-05 22:10:41 +0800

    finish update parser

--- nailit/src/nailit/logic/parser/UpdateParser.java
+++ nailit/src/nailit/logic/parser/UpdateParser.java
@@ -1,6 +1,7 @@
 package nailit.logic.parser;
 
 import nailit.common.NIConstants;
+import nailit.common.TaskPriority;
 import nailit.logic.CommandType;
 import nailit.logic.ParserResult;
 
@@ -18,15 +19,29 @@ public class UpdateParser extends Parser {
 		ParserResult resultExecution = new ParserResult();
 		
 		resultExecution.setCommand(CommandType.UPDATE);
-		listOfCommand = userCommand.split(NIConstants.FIELD_SPLITTER);
+		listOfCommand = userCommand.split(NIConstants.UPDATE_FIELD_SPLITTER);
 		
-		if (userCommand.equalsIgnoreCase("ALL")){
-			resultExecution.setDisplayAll(true);
-		}else if (Parser.isTaskID(userCommand)){
-			resultExecution.setTaskID(Integer.parseInt(userCommand));
-		}else{
-			resultExecution.setStartTime(Parser.retrieveDateTime(userCommand));
+		if (listOfCommand[0].equalsIgnoreCase("name")){
+			String answer = "";
+			for (int i=1; i<listOfCommand.length; i++)
+				answer += listOfCommand[i];
+			resultExecution.setName(answer);
+		}else if (listOfCommand[0].equalsIgnoreCase("tag")){
+			resultExecution.setTag(listOfCommand[1]);
+		}else if (listOfCommand[0].equalsIgnoreCase("StartTime")){
+			String answer = "";
+			for (int i=1; i<listOfCommand.length; i++)
+				answer += listOfCommand[i];
+			resultExecution.setStartTime(Parser.retrieveDateTime(answer));
+		}else if (listOfCommand[0].equalsIgnoreCase("EndTime")){
+			String answer = "";
+			for (int i=1; i<listOfCommand.length; i++)
+				answer += listOfCommand[i];
+			resultExecution.setEndTime(Parser.retrieveDateTime(answer));
+		}else if (listOfCommand[0].equalsIgnoreCase("Priority")){
+			resultExecution.setPriority(TaskPriority.valueOf(listOfCommand[1]));
 		}
+		
 		return resultExecution;
 	}
 }
