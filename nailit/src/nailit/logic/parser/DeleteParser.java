7c7afe13390c5d88dfa4a3b8944ae8a8144ad571
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-10-05 16:50:15 +0800

    add one more field in parser result

--- nailit/src/nailit/logic/parser/DeleteParser.java
+++ nailit/src/nailit/logic/parser/DeleteParser.java
@@ -1,6 +1,7 @@
 package nailit.logic.parser;
 
 import nailit.common.NIConstants;
+import nailit.logic.CommandType;
 import nailit.logic.ParserResult;
 
 public class DeleteParser extends Parser {
@@ -17,6 +18,7 @@ public class DeleteParser extends Parser {
 		ParserResult resultExecution = new ParserResult();
 		listOfCommand = userCommand.split(NIConstants.FIELD_SPLITTER);
 	
+		resultExecution.setCommand(CommandType.DELETE);
 		resultExecution.setTaskID(Integer.parseInt(userCommand));
 		
 		return resultExecution;
