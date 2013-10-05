4f1bd7e85580f41e2f1a742427c3b5886a7a1987
Shuzhi <nusszyu@gmail.com>
2013-10-05 10:27:28 +0800

    change something in class Task: delete the argument description in the constructor, since the desc is added later. Change something on class ParserResult: type for priority is TASK_PRIORITY

--- nailit/src/nailit/logic/command/CommandAdd.java
+++ nailit/src/nailit/logic/command/CommandAdd.java
@@ -1,30 +1,46 @@
 package nailit.logic.command;
 
+import java.util.Date;
+
 import nailit.common.Result;
+import nailit.common.Task;
+import nailit.logic.CommandType;
 import nailit.logic.ParserResult;
 import nailit.storage.StorageManager;
 
 public class CommandAdd extends Command{
 	private Result executedResult;
+	private Task taskPassedToStorer;
+	private CommandType command;
+	private String taskName;
+	private Date startTime;
+	private Date endTime;
+	private Integer taskPriority;
+	private String taskTag;
 	
 	// constructor
 	public CommandAdd(ParserResult ResultInstance, StorageManager storerToUse) {
 		super(ResultInstance, storerToUse);
 	}
-	
-	public Result execute() {
-		doExecution();
-		return executedResult;
-	}
-	
-	private void doExecution() {
-		
-	}
 
 	@Override
 	public Result executeCommand(ParserResult parserResultInstance) {
-		// TODO Auto-generated method stub
+		getContentFromParserResult();
+		createTaskObject();
 		return null;
+	}
+
+	private void createTaskObject() {
+		taskPassedToStorer = new Task(taskName, startTime, endTime, taskTag, taskPriority);
+	}
+
+	private void getContentFromParserResult() {
+		command = parserResultInstance.getCommand();
+		taskName = parserResultInstance.getName();
+		startTime = parserResultInstance.getStartTime();
+		endTime = parserResultInstance.getEndTime();
+		taskPriority = parserResultInstance.getPriority();
+		taskTag = parserResultInstance.getTag();
 	}
 	
 }
