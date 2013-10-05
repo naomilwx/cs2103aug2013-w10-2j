0306aced6744a8128226d667f6427bbb0edc3202
Wei Wenbo <weiwenbo3@yahoo.com.sg>
2013-10-05 13:47:58 +0800

    move the FIELD_SPLITTER to the NIContants

---
+++ nailit/src/nailit/common/Task.java
@@ -0,0 +1,276 @@
+package nailit.common;
+
+import org.joda.time.DateTime;
+public class Task {
+	public static final int TASKID_NULL = -1;
+	private static final String BASIC_PRINTOUT_FORMAT = "[%1d] Name: %2s";
+	private int ID;
+	private String name;
+	private String description;
+	private DateTime startTime;
+	private DateTime endTime;
+	private String tag;
+	private boolean added = false; //variable to indicate if task has been added to task list
+	private boolean isCompleted;
+
+	private TaskPriority priority;
+
+	public static final int LOW_IN_HARDDISK = 0;
+	public static final int MEDIUM_IN_HARDDISK = 1;
+	public static final int HIGH_IN_HARDDISK =2;
+	public static final int INVALID_PRIORITY_IN_HARDDISK = -1;
+	public static final int COMPLETED_IN_HARDDISK = 1;
+	public static final int INCOMPLETE_IN_HARDDISK = 0;
+
+	public Task(){
+		ID = TASKID_NULL;
+		name = "";
+		description = ""; //TODO: replace with constant
+		startTime = null;
+		endTime = null;
+		tag = "";
+		isCompleted = false;
+		priority = null;
+	}
+	public Task(String taskName){
+		ID = TASKID_NULL;
+		name = taskName;
+		description = ""; //TODO: replace with constant
+		startTime = null;
+		endTime = null;
+		tag = "";
+		isCompleted = false;
+		added = true;
+		priority = TaskPriority.MEDIUM;
+	}
+	
+	public Task(String taskName, DateTime start, DateTime end, String t, TaskPriority p){
+		ID = TASKID_NULL;
+		name = taskName;
+		startTime = start;
+		endTime = end;
+		tag = t;
+		isCompleted = false;
+		priority = p;
+	}
+	
+	public boolean isAdded(){
+		return added;
+	}
+	//getters
+	public int getID(){
+		return ID;
+	}
+	public TaskPriority getPriority(){
+		return priority;
+	}
+	public String getName(){
+		return name;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	public String getTag(){
+		return tag;
+	}
+	public boolean checkCompleted(){
+		return isCompleted;
+	}
+	public DateTime getStartTime(){
+		return startTime;
+	}
+	public DateTime getEndTime(){
+		return endTime;
+	}
+	public boolean isFloatingTask(){
+		if(startTime==null && endTime==null){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//Checks if task is an event. returns true if both startTime and endTime are not null
+	public boolean isEvent(){
+		if(startTime != null && endTime != null){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//setters
+	public boolean setID(int newID){
+		if(ID==Task.TASKID_NULL){
+			ID = newID;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public void setName(String taskName){
+		name = taskName;
+	}
+	public void setDescription(String taskDesc){
+		description = taskDesc;
+	}
+	public void setTag(String taskTag){
+		tag = taskTag;
+	}
+	public void setCompleted(boolean completed){
+		isCompleted = completed;
+	}
+	public void setPriority(TaskPriority taskPriority){
+		priority = taskPriority;
+	}
+	public void setStartTime(DateTime start){
+		startTime = start;
+	}
+	public void setEndTime(DateTime end){
+		endTime = end;
+	}
+	//Functions to format Task details for printout
+	public String formatID(){
+		String formattedID = "ID: "+ ID;
+		return formattedID;
+	}
+	public String formatName(){
+		String formattedName = "Name: " + name;
+		return formattedName;
+	}
+	public String formatTag(){
+		String formattedTag = "";
+		if(!tag.isEmpty()){
+			formattedTag = "Tag: " + tag;
+		}
+		return formattedTag;
+	}
+	public String formatPriority(){
+		String formattedPriority = "Priority: " + priority.toString();
+		return formattedPriority;
+	}
+	public String formatDateDetails(){
+		String formattedDateDetails = "";
+		if(isEvent()){
+			formattedDateDetails = "Start Time: " + startTime + "\n"
+									+"End Time: " + endTime;
+		}else{
+			formattedDateDetails = "Due: " + startTime;
+		}
+		return formattedDateDetails;
+	}
+	public String formatStatus(){
+		String formattedStatus = "Status: ";
+		if(isCompleted){
+			formattedStatus += "Completed";
+		}else{
+			formattedStatus += "Not Completed";
+		}
+		return formattedStatus;
+	}
+	//method to create copy of Task object
+	public Task copy(){
+		// change from Shuzhi
+		// the same, delete description,
+		Task newTask = new Task(name, startTime, endTime, tag, priority);
+		newTask.setDescription(description);
+		newTask.setID(ID);
+		newTask.setCompleted(isCompleted);
+		return newTask;
+	}
+	public String printNameAndID(){
+		return String.format(BASIC_PRINTOUT_FORMAT, ID, name);
+	}
+	public boolean isAtSameStartTime(Task other){
+		if(startTime == null || other.startTime == null){
+			return ((startTime == null) && (other.startTime ==null));
+		}
+		return startTime.equals(other.startTime);
+	}
+	public boolean isAtSameEndTime(Task other){
+		if(endTime == null || other.endTime == null){
+			return ((endTime == null) && (other.endTime ==null));
+		}
+		return endTime.equals(other.endTime);
+	}
+	public boolean isAtSameTime(Task other){
+		return (isAtSameStartTime(other) && isAtSameEndTime(other));
+	}
+	@Override
+	public String toString(){ //TODO: figure out whether to include description
+		String output = formatID()+"\n"
+						+formatName() +"\n";
+		if(!isFloatingTask()){
+			output += formatDateDetails() + "\n";
+		}
+		output += formatPriority() +"\n"
+				 + formatStatus() + "\n";
+		if(!tag.isEmpty()){
+			output += formatTag() + "\n";
+		}
+		return output;
+	}
+	@Override
+	public boolean equals(Object otherTask){
+		if(otherTask instanceof Task){
+			int otherTaskID = ((Task) otherTask).getID();
+			if(otherTaskID == Task.TASKID_NULL){
+				return false;
+			}else{
+				return otherTaskID == ID;
+			}
+		}else{
+			return false;
+		}
+	}
+	public boolean isClone(Object otherTask){
+		if(this.equals(otherTask)){
+			String otherTaskString = otherTask.toString();
+			return otherTaskString.equals(this.toString());
+		}else{
+			return false;
+		}
+	}
+	
+	public String changeToDiskFormat(){
+		int priority = parsePriority(this.getPriority());
+		assert(isValidPriority(priority));
+		
+		String name = this.getName();
+		String startDate = this.getStartTime().toString();
+		String endDate = this.getEndTime().toString();
+		String desc = this.getDescription();
+		String tag = this.getTag();
+		
+		int completeStatus = parseCompleteStatus(this.isCompleted);
+		assert(isValidCompleteStatus(completeStatus));
+		
+		
+		String taskString = name + NIConstants.FIELD_SPLITTER + startDate + NIConstants.FIELD_SPLITTER + endDate + NIConstants.FIELD_SPLITTER + priority + NIConstants.FIELD_SPLITTER + tag + NIConstants.FIELD_SPLITTER +desc +NIConstants.FIELD_SPLITTER + completeStatus;
+		
+		return taskString;
+	}
+	
+	private int parsePriority(TaskPriority p){
+		switch(p){
+			case LOW: return LOW_IN_HARDDISK;
+			case MEDIUM: return MEDIUM_IN_HARDDISK;
+			case HIGH: return HIGH_IN_HARDDISK;
+			default: return INVALID_PRIORITY_IN_HARDDISK;
+		}
+	}
+	private int parseCompleteStatus(boolean complete){
+		if(complete){
+			return COMPLETED_IN_HARDDISK;
+		}
+		else{
+			return INCOMPLETE_IN_HARDDISK;
+		}
+	}
+	private boolean isValidPriority(int p){
+		return p>=LOW_IN_HARDDISK&&p<=HIGH_IN_HARDDISK;
+	}
+	private boolean isValidCompleteStatus(int completeStatus){
+		return ((completeStatus == COMPLETED_IN_HARDDISK) || (completeStatus == INCOMPLETE_IN_HARDDISK));
+	}
+
+}
---
+++ nailit/src/nailit/logic/ParserResult.java
@@ -0,0 +1,103 @@
+package nailit.logic;
+
+import java.util.*;
+
+import org.joda.time.DateTime;
+
+import nailit.common.TaskPriority;
+
+public class ParserResult{
+	//ArrayList<String> command = new ArrayList<String> ();
+
+	// added by Shuzhi
+	// in delete and addDescription command: need tasdID
+	private int taskID;
+	private static CommandType command;
+	private static String name;
+	
+	private DateTime startTime;
+	private DateTime endTime;
+	
+	private TaskPriority priority;
+	private String tag;
+	
+	public void setCommand(CommandType commandExternal){
+		command = commandExternal;
+	}
+	
+	public CommandType getCommand(){
+		return command;
+	}
+	
+	public void setName(String nameExternal){
+		name = nameExternal;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public boolean isNullName(){
+		if (name == ""){
+			return true;
+		}else{ 
+			return false;
+		}
+	}
+	
+	public void setTaskID(int taskIDExternal) {
+		taskID = taskIDExternal;
+	}
+	
+	public int getTaskID() {
+		return taskID;
+	}
+
+	public void setStartTime(DateTime startTimeExternal){
+		startTime = startTimeExternal;
+	}
+	
+	public DateTime getStartTime(){
+		return startTime;
+	}
+	
+	public void setEndTime(DateTime endTimeExternal){
+		endTime = endTimeExternal;
+	}
+	
+	public DateTime getEndTime(){
+		return endTime;
+	}
+	
+	public void setPriority(TaskPriority priorityExternal){
+		priority = priorityExternal;
+	}
+	
+	public TaskPriority getPriority(){
+		return priority;
+	}
+	
+	public boolean isNullPriority(){
+		if (priority == null){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void setTag(String tagExternal){
+		tag = tagExternal;
+	}
+	
+	public String getTag(){
+		return tag;
+	}
+	
+	public boolean isNullTag(){
+		if (tag == ""){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
