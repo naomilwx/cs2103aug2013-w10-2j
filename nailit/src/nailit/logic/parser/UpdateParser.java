0030d669318d4d0df3e505f33ac3f3416271957f
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-10-05 21:53:06 +0800

    distinguish the add and update field splitter

--- nailit/src/nailit/logic/parser/UpdateParser.java
+++ nailit/src/nailit/logic/parser/UpdateParser.java
@@ -1,11 +1,13 @@
 package nailit.logic.parser;
 
+import nailit.common.NIConstants;
 import nailit.logic.CommandType;
 import nailit.logic.ParserResult;
 
 public class UpdateParser extends Parser {
 
 	private String userCommand;
+	private String[] listOfCommand;
 	
 	public UpdateParser (String command){
 		userCommand = command;
@@ -16,6 +18,7 @@ public class UpdateParser extends Parser {
 		ParserResult resultExecution = new ParserResult();
 		
 		resultExecution.setCommand(CommandType.UPDATE);
+		listOfCommand = userCommand.split(NIConstants.FIELD_SPLITTER);
 		
 		if (userCommand.equalsIgnoreCase("ALL")){
 			resultExecution.setDisplayAll(true);
