104362dd1659a15c45800800ade6ee08c2fc10c8
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-10-06 09:24:22 +0800

    complete showhistory parser

--- nailit/src/nailit/logic/parser/ShowHistoryParser.java
+++ nailit/src/nailit/logic/parser/ShowHistoryParser.java
@@ -1,5 +1,26 @@
 package nailit.logic.parser;
 
-public class ShowHistoryParser {
+import nailit.common.NIConstants;
+import nailit.logic.CommandType;
+import nailit.logic.ParserResult;
 
+public class ShowHistoryParser extends Parser {
+	
+	private String userCommand;
+	private String[] listOfCommand;
+	
+	public ShowHistoryParser (String command){
+		userCommand = command;
+	}
+	
+	@Override
+	public ParserResult execute(){
+		ParserResult resultExecution = new ParserResult();
+		listOfCommand = userCommand.split(NIConstants.NORMAL_FIELD_SPLITTER);
+	
+		resultExecution.setCommand(CommandType.SHOWHISTORY);
+		resultExecution.setTaskID(Integer.parseInt(userCommand));
+		
+		return resultExecution;
+	}
 }
