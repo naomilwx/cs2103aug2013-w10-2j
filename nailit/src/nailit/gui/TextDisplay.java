f38b002c3da0558ccd69f81565587627e79b2915
Naomi Leow <naomileowwx@gmail.com>
2013-10-07 19:09:19 +0800

    fixed the keylistener bug. changed line border on focus

--- nailit/src/nailit/gui/TextDisplay.java
+++ nailit/src/nailit/gui/TextDisplay.java
@@ -1,5 +1,8 @@
 package nailit.gui;
 
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.border.LineBorder;
@@ -7,10 +10,23 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 
 public class TextDisplay extends JScrollPane{
+	private final LineBorder FOCUS_LINE_BORDER = new LineBorder(GUIManager.FOCUSED_BORDER_COLOR);
+	private final LineBorder UNFOCUS_LINE_BORDER = new LineBorder(GUIManager.BORDER_COLOR);
+	private final FocusListener textDisplayFocusListener = new FocusListener(){
+		public void focusGained(FocusEvent event) {
+			setBorder(FOCUS_LINE_BORDER);
+		 }
+		public void focusLost(FocusEvent event){
+			setBorder(UNFOCUS_LINE_BORDER);
+		}
+	};
+	
 	protected JTextPane textPane;
+	
 	public TextDisplay(int width, int height){
 		setSize(width, height);
-		setBorder(new LineBorder(GUIManager.BORDER_COLOR));
+		setBorder(UNFOCUS_LINE_BORDER);
+		addFocusListener(textDisplayFocusListener);
 		createAndConfigureTextPane();
 	}
 	private void createAndConfigureTextPane(){
