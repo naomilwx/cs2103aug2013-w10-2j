110e652408ee77e8d954d199446cace45f825ffc
Wei Wenbo <weiwenbo3@yahoo.com.sg>
2013-10-07 19:54:25 +0800

    add retrieve all function

---
+++ nailit/src/nailit/common/Task.java
@@ -0,0 +1,289 @@
+package nailit.common;
+
+import nailit.common.NIConstants;
+
+import org.joda.time.DateTime;
+public class Task {
+	public static final int TASKID_NULL = -1;
+	private static final String BASIC_PRINTOUT_FORMAT = "[%1d] Name: %2s";
+	
+	private int ID;
+	private String name;
+	private String description;
+	private DateTime startTime;
+	private DateTime endTime;
+	private String tag;
+	private boolean added = false; //variable to indicate if task has been added to task list
+	private boolean isCompleted;
+
+	private TaskPriority priority;
+
+	public static final int COMPLETED_IN_HARDDISK = 1;
+	public static final int INCOMPLETE_IN_HARDDISK = 0;
+
+	public Task(){
+		ID = TASKID_NULL;
+		name = "";
+		description = ""; //TODO: replace with constant
+		startTime = null;
+		endTime = null;
+		tag = "";
+		isCompleted = false;
+		priority = TaskPriority.DEFAULT_TASK_PRIORITY;
+	}
+	public Task(String taskName){
+		ID = TASKID_NULL;
+		name = taskName;
+		description = ""; //TODO: replace with constant
+		startTime = null;
+		endTime = null;
+		tag = "";
+		isCompleted = false;
+		added = true;
+		priority = TaskPriority.DEFAULT_TASK_PRIORITY;
+	}
+	
+	public Task(String taskName, DateTime start, DateTime end, String t, TaskPriority p){
+		ID = TASKID_NULL;
+		name = taskName;
+		startTime = start;
+		endTime = end;
+		tag = t;
+		isCompleted = false;
+		priority = p;
+	}
+	
+	public Task(int ID, String taskName, DateTime start, DateTime end, TaskPriority p, String tag, String desc, boolean isCompleted){
+		this.ID = ID;
+		name = taskName;
+		startTime = start;
+		endTime = end;
+		priority = p;
+		this.tag = tag;
+		description = desc;
+		this.isCompleted = isCompleted;
+	}
+	public boolean isAdded(){
+		return added;
+	}
+	//getters
+	public int getID(){
+		return ID;
+	}
+	public TaskPriority getPriority(){
+		return priority;
+	}
+	public String getName(){
+		return name;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	public String getTag(){
+		return tag;
+	}
+	public boolean checkCompleted(){
+		return isCompleted;
+	}
+	public DateTime getStartTime(){
+		return startTime;
+	}
+	public DateTime getEndTime(){
+		return endTime;
+	}
+	public boolean isFloatingTask(){
+		if(startTime==null && endTime==null){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//Checks if task is an event. returns true if both startTime and endTime are not null
+	public boolean isEvent(){
+		if(startTime != null && endTime != null){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//setters
+	public boolean setID(int newID){
+		if(ID==Task.TASKID_NULL){
+			ID = newID;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public void setName(String taskName){
+		name = taskName;
+	}
+	public void setDescription(String taskDesc){
+		description = taskDesc;
+	}
+	public void setTag(String taskTag){
+		tag = taskTag;
+	}
+	public void setCompleted(boolean completed){
+		isCompleted = completed;
+	}
+	public void setPriority(TaskPriority taskPriority){
+		priority = taskPriority;
+	}
+	public void setStartTime(DateTime start){
+		startTime = start;
+	}
+	public void setEndTime(DateTime end){
+		endTime = end;
+	}
+	//Functions to format Task details for printout
+	public String formatID(){
+		String formattedID = "ID: "+ ID;
+		return formattedID;
+	}
+	public String formatName(){
+		String formattedName = "Name: " + name;
+		return formattedName;
+	}
+	public String formatTag(){
+		String formattedTag = "";
+		if(!tag.isEmpty()){
+			formattedTag = "Tag: " + tag;
+		}
+		return formattedTag;
+	}
+	public String formatPriority(){
+		String formattedPriority = "Priority: " + priority.toString();
+		return formattedPriority;
+	}
+	public String formatDateDetails(){
+		String formattedDateDetails = "";
+		if(isEvent()){
+			formattedDateDetails = 
+					"Start Time: " + startTime.toString(NIConstants.DISPLAY_FULL_DATETIME_FORMAT) + "\n"
+					+"End Time: " + endTime.toString(NIConstants.DISPLAY_FULL_DATETIME_FORMAT);
+		}else if(!isFloatingTask()){
+			assert startTime != null;	//non floating tasks should not have null start time
+			formattedDateDetails = "Due: " + startTime.toString(NIConstants.DISPLAY_FULL_DATETIME_FORMAT);
+		}
+		return formattedDateDetails;
+	}
+	public String formatStatus(){
+		String formattedStatus = "Status: ";
+		if(isCompleted){
+			formattedStatus += "Completed";
+		}else{
+			formattedStatus += "Not Completed";
+		}
+		return formattedStatus;
+	}
+	//method to create copy of Task object
+	public Task copy(){
+		// change from Shuzhi
+		// the same, delete description,
+		Task newTask = new Task(name, startTime, endTime, tag, priority);
+		newTask.setDescription(description);
+		newTask.setID(ID);
+		newTask.setCompleted(isCompleted);
+		return newTask;
+	}
+	public String printNameAndID(){
+		return String.format(BASIC_PRINTOUT_FORMAT, ID, name);
+	}
+	public boolean isAtSameStartTime(Task other){
+		if(startTime == null || other.startTime == null){
+			return ((startTime == null) && (other.startTime ==null));
+		}
+		return startTime.equals(other.startTime);
+	}
+	public boolean isAtSameEndTime(Task other){
+		if(endTime == null || other.endTime == null){
+			return ((endTime == null) && (other.endTime ==null));
+		}
+		return endTime.equals(other.endTime);
+	}
+	public boolean isAtSameTime(Task other){
+		return (isAtSameStartTime(other) && isAtSameEndTime(other));
+	}
+	//check if task is an event and startTime and endTime is on the same day
+	public boolean isOneDayEvent(){
+		if(!isEvent()){
+			return false;
+		}else{
+			String startDay = startTime.toString(NIConstants.DISPLAY_DATE_FORMAT);
+			String endDay = endTime.toString(NIConstants.DISPLAY_DATE_FORMAT);
+			return startDay.equals(endDay);
+		}
+	}
+	@Override
+	public String toString(){ //TODO: figure out whether to include description
+		String output = formatID()+"\n"
+						+formatName() +"\n";
+		if(!isFloatingTask()){
+			output += formatDateDetails() + "\n";
+		}
+		output += formatPriority() +"\n"
+				 + formatStatus() + "\n";
+		if(!tag.isEmpty()){
+			output += formatTag() + "\n";
+		}
+		return output;
+	}
+	@Override
+	public boolean equals(Object otherTask){
+		if(otherTask instanceof Task){
+			int otherTaskID = ((Task) otherTask).getID();
+			if(otherTaskID == Task.TASKID_NULL){
+				return false;
+			}else{
+				return otherTaskID == ID;
+			}
+		}else{
+			return false;
+		}
+	}
+	public boolean isClone(Object otherTask){
+		if(this.equals(otherTask)){
+			String otherTaskString = otherTask.toString();
+			return otherTaskString.equals(this.toString());
+		}else{
+			return false;
+		}
+	}
+	
+	public String changeToDiskFormat(){
+		int priority = this.getPriority().getPriorityCode();
+		assert(isValidPriority(priority));
+		
+		String name = this.getName();
+		String startDate = this.getStartTime().toString();
+		String endDate = this.getEndTime().toString();
+		String desc = this.getDescription();
+		String tag = this.getTag();
+		
+		int completeStatus = parseCompleteStatus(this.isCompleted);
+		assert(isValidCompleteStatus(completeStatus));
+		
+		
+		String taskString = name + NIConstants.NORMAL_FIELD_SPLITTER + startDate + NIConstants.NORMAL_FIELD_SPLITTER + endDate + NIConstants.NORMAL_FIELD_SPLITTER + priority + NIConstants.NORMAL_FIELD_SPLITTER + tag + NIConstants.NORMAL_FIELD_SPLITTER +desc +NIConstants.NORMAL_FIELD_SPLITTER + completeStatus;
+		
+		return taskString;
+	}
+	
+	private int parseCompleteStatus(boolean complete){
+		if(complete){
+			return COMPLETED_IN_HARDDISK;
+		}
+		else{
+			return INCOMPLETE_IN_HARDDISK;
+		}
+	}
+	private boolean isValidPriority(int p){
+		return (p>=TaskPriority.LOW.getPriorityCode()&&p<=TaskPriority.HIGH.getPriorityCode());
+	}
+	private boolean isValidCompleteStatus(int completeStatus){
+		return ((completeStatus == COMPLETED_IN_HARDDISK) || (completeStatus == INCOMPLETE_IN_HARDDISK));
+	}
+
+}
---
+++ nailit/src/nailit/gui/GUIManager.java
@@ -0,0 +1,184 @@
+package nailit.gui;
+
+import nailit.AppLauncher;
+
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.BorderLayout;
+import java.awt.Point;
+
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.util.Vector;
+
+import javax.swing.border.LineBorder;
+import javax.swing.text.Utilities;
+
+import test.logic.LogicManagerStub;
+import nailit.common.Result;
+import nailit.common.Task;
+import nailit.logic.LogicManager;
+
+public class GUIManager {	
+	public static final String APPLICATION_NAME = "NailIt!";
+	protected static final Color BORDER_COLOR = Color.black;
+	protected static final Color FOCUSED_BORDER_COLOR = Color.orange;
+	protected static final int Y_BUFFER_HEIGHT = 10;
+	protected static final int X_BUFFER_WIDTH = 5;
+	protected static final int WINDOW_RIGHT_BUFFER = 12;
+	protected static final int WINDOW_BOTTOM_BUFFER = 35;
+	protected static final int MAIN_WINDOW_X_POS = 100;
+	protected static final int MAIN_WINDOW_Y_POS = 100;
+	protected static final String DEFAULT_WINDOW_LOOKANDFEEL = "javax.swing.plaf.metal.MetalLookAndFeel";
+	protected static final Point DEFAULT_COMPONENT_LOCATION = new Point(0, 0);
+	private static final String WELCOME_MESSAGE = "Welcome to NailIt!";
+	
+	protected static final int ID_COLUMN_WIDTH = 50;
+	protected static final int NAME_COLUMN_WIDTH = 300;
+	protected static final int TIME_COLUMN_WIDTH = 180;
+	protected static final int TAG_COLUMN_WIDTH = 80;
+	protected static final int STATUS_COLUMN_WIDTH = 50;
+	protected static final int TOTAL_TABLE_WIDTH
+	= ID_COLUMN_WIDTH + NAME_COLUMN_WIDTH + TIME_COLUMN_WIDTH + STATUS_COLUMN_WIDTH;
+	protected static final int COMMAND_COLUMN_WIDTH = TOTAL_TABLE_WIDTH - ID_COLUMN_WIDTH;
+	protected static final int[] TASKS_TABLE_COLUMN_WIDTH = 
+		{ID_COLUMN_WIDTH, NAME_COLUMN_WIDTH, TIME_COLUMN_WIDTH, STATUS_COLUMN_WIDTH};
+	protected static final int[] COMMAND_HISTORY_COLUMN_WIDTH =
+		{ID_COLUMN_WIDTH, COMMAND_COLUMN_WIDTH};
+	
+	protected static final String ID_COL_NAME = "ID";
+	protected static final String TASK_NAME_COL_NAME = "Task Name";
+	protected static final String TASK_TIME_DET_COL_NAME = "Time";
+	protected static final String TASK_STATUS_COL_NAME = "Status";
+	protected static final String COMMAND_COL_NAME = "Command";
+	protected static final String[] ALL_TASKS_TABLE_HEADER = 
+		{ID_COL_NAME, TASK_NAME_COL_NAME, TASK_TIME_DET_COL_NAME, TASK_STATUS_COL_NAME};
+	protected static final String[] COMMAND_HISTORY_HEADER =
+		{ID_COL_NAME, COMMAND_COL_NAME};
+	
+	private MainWindow mainWindow;
+	private CommandBar commandBar;
+	private DisplayArea displayArea;
+	private NotificationArea notificationArea;
+	
+	private AppLauncher launcher;
+	private LogicManager logicExecutor;
+	//TODO:
+	private LogicManagerStub logicstub;
+	
+	public GUIManager(final AppLauncher launcher) {
+		setWindowLookAndFeel();
+		this.launcher = launcher;
+		//TODO:
+//		logicExecutor = new LogicManager();
+		logicstub = new LogicManagerStub();
+		createComponentsAndAddToMainFrame();
+		//stub to be modified later
+	}
+	
+	private void createComponentsAndAddToMainFrame() {
+		mainWindow = new MainWindow(this);
+		commandBar = new CommandBar(this, mainWindow.getWidth(), mainWindow.getHeight());
+		initialiseAndConfigureDisplayArea();
+		loadComponentsUntoMainFrame();
+	}
+	private void initialiseAndConfigureDisplayArea(){
+		displayArea = new DisplayArea(this, mainWindow.getWidth(), mainWindow.getHeight());
+		notificationArea = new NotificationArea(displayArea.getWidth());
+		notificationArea.displayNotification(GUIManager.WELCOME_MESSAGE, true);
+		displayArea.addPopup(notificationArea);
+	}
+	private void loadComponentsUntoMainFrame(){
+		mainWindow.addItem(commandBar);
+		mainWindow.addItem(displayArea);
+	}
+	public void setVisible(boolean isVisible){
+		mainWindow.setVisible(isVisible);
+	}
+	
+	public void setFocusOnDisplay(){
+		displayArea.setFocus();
+	}
+	public void setFocusOnCommandBar(){
+		commandBar.setFocus();
+	}
+	/**
+	 * Executes command entered by user
+	 * @param input
+	 */
+	protected void executeUserInputCommand(String input){
+		System.out.println(input);
+		displayArea.hideNotifications();
+		Result executionResult = logicstub.executeCommand(input);
+		commandBar.clearUserInput();
+		processAndDisplayExecutionResult(executionResult);
+//		logicExecutor.executeCommand(input);
+	}
+	protected void processAndDisplayExecutionResult(Result result){
+		if(result.getExitStatus()){
+			exit();
+		}else{
+			displayExecutionResult(result);
+		}
+	}
+	protected void displayExecutionResult(Result result){
+		int displayType = result.getDisplayType();
+		switch (displayType){
+			case Result.TASK_DISPLAY:
+				displayTaskDetails(result.getTaskList());
+				break;
+			case Result.LIST_DISPLAY:
+				displayArea.displayTaskList(result.getTaskList());
+				break;
+			case Result.HISTORY_DISPLAY:
+				displayArea.displayHistoryList(result.getHistoryList());
+				break;
+			default:
+				displayExecutionNotification(result);
+				break;
+		}
+	}
+	private void displayExecutionNotification(Result result){
+		String notificationStr = result.getPrintOut();
+		boolean isSuccess = result.getExecutionSuccess();
+		if(!notificationStr.isEmpty()){
+			notificationArea.displayNotification(notificationStr, isSuccess);
+		}
+		displayArea.showNotifications();
+	}
+	private void displayTaskDetails(Vector<Task> taskList){
+		assert taskList.size() > 0;
+		Task task = taskList.firstElement();
+		String taskDisplay = task.toString();
+		displayArea.displayTaskDetails(taskDisplay);
+	}
+
+	private void exit(){
+		launcher.exit();
+	}
+
+	private void setWindowLookAndFeel(){
+		try {
+			UIManager.setLookAndFeel(DEFAULT_WINDOW_LOOKANDFEEL);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
