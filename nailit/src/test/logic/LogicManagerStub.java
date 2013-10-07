ae808f263aefeac4bb463ccad81e9520d432dd60
Shuzhi <nusszyu@gmail.com>
2013-10-07 19:39:52 +0800

    finish CommandDelete test

---
+++ nailit/src/test/logic/LogicManagerStub.java
@@ -0,0 +1,38 @@
+package test.logic;
+
+import java.util.Vector;
+
+import org.joda.time.DateTime;
+
+import nailit.common.Result;
+import nailit.common.Task;
+import nailit.common.TaskPriority;
+import nailit.logic.LogicManager;
+import nailit.logic.command.CommandManager;
+
+public class LogicManagerStub{//NOTE should extend LogicManager. standalone for now because of bugs downstream
+	private DateTime startTime = new DateTime(2013,9,30,10,20);
+	private DateTime endTime = new DateTime();
+	private Task event = new Task("New Event Test", startTime, endTime, "#stub", TaskPriority.MEDIUM);
+	private Task task = new Task("New Task Test", startTime, null, "#test", TaskPriority.HIGH);
+	private Vector<Task> taskList = new Vector<Task>();
+	private Result res = new Result();
+	
+	public LogicManagerStub(){
+		taskList.add(event);
+		taskList.add(task);
+	}
+	public Result executeCommand(String command){
+		res.setTaskList(taskList);
+		res.setDisplayType(Result.NOTIFICATION_DISPLAY);
+		res.setNotification("test");
+		if(command.equals("exit")){
+			res.setExitStatus(true);
+		}else if(command.equals("task")){
+			res.setDisplayType(Result.TASK_DISPLAY);
+		}else if(command.equals("list")){
+			res.setDisplayType(Result.LIST_DISPLAY);
+		}
+		return res;
+	}
+}
