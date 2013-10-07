c006747cd4f4bf239d9802cb9644b8528973b85f
Shuzhi <nusszyu@gmail.com>
2013-10-07 18:35:29 +0800

    commit for the later push, currently implementing display all

--- nailit/src/nailit/logic/command/CommandDisplay.java
+++ nailit/src/nailit/logic/command/CommandDisplay.java
@@ -14,31 +14,48 @@ public class CommandDisplay extends Command{
 	private Task taskRetrieved;
 	private int taskToRetrieveID;
 	
+	// the CommandManager instance that the CommandDisplay 
+	// instance belongs to, since it may display the operations
+	// history
+	private CommandManager cm;
+	
 	private final String UnsuccessfulFeedback = "Sorry, there is no such task record in the storage, please check and try again.";
 
 //	private final String Success_Msg = "The task is deleted successfully, the Task ID for it is: ";;
 	public CommandDisplay(ParserResult resultInstance,
-			StorageManager storerToUse) {
+			StorageManager storerToUse, CommandManager cm) {
 		super(resultInstance, storerToUse);
+		this.cm = cm;
 		commandType = "display";
 	}
 
 	@Override
 	public Result executeCommand() {
+		if(parserResultInstance.isDisplayAll()) {
+			retrieveAllTheTasks();
+		} else if(parserResultInstance.isDisplayHistory()) {
+			
+		}
 		retrieveTheTask();
-		createResultObject();
+		//createResultObject(false, true, Result.TASK_DISPLAY, null, vectorStoringTheTask, null);
 		createCommandSummary();
 		return executedResult;
+	}
+
+	private void retrieveAllTheTasks() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private void createCommandSummary() {
 		commandSummary = "display the task with the Task ID: " + taskToRetrieveID;		
 	}
 
-	private void createResultObject() {
+	private void createResultObject(boolean isExitCommand, boolean isSuccess, int displayType, 
+			String printOut, Vector<Task> tasks, Vector<String> history) {
 		Vector<Task> vectorStoringTheTask = new Vector<Task>();
 		vectorStoringTheTask.add(taskRetrieved);
-		executedResult = new Result(false, true, Result.TASK_DISPLAY, null, vectorStoringTheTask, null);
+		executedResult = new Result(isExitCommand, isSuccess, displayType, printOut, tasks, history);
 	}
 	
 	// there is no such task record in the storage to display
