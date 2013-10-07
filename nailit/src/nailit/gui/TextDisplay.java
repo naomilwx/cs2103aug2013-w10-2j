e9df630c12cc056958bd64fed1aa5bbe6990abf9
Naomi Leow <naomileowwx@gmail.com>
2013-10-07 20:17:41 +0800

    refactored TextDisplay and TableDisplay. created an abstract class for both classes

--- nailit/src/nailit/gui/TextDisplay.java
+++ nailit/src/nailit/gui/TextDisplay.java
@@ -9,17 +9,17 @@ import javax.swing.border.LineBorder;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 
-public class TextDisplay extends JScrollPane{
-	private final LineBorder FOCUS_LINE_BORDER = new LineBorder(GUIManager.FOCUSED_BORDER_COLOR);
-	private final LineBorder UNFOCUS_LINE_BORDER = new LineBorder(GUIManager.BORDER_COLOR);
-	private final FocusListener textDisplayFocusListener = new FocusListener(){
-		public void focusGained(FocusEvent event) {
-			setBorder(FOCUS_LINE_BORDER);
-		 }
-		public void focusLost(FocusEvent event){
-			setBorder(UNFOCUS_LINE_BORDER);
-		}
-	};
+public class TextDisplay extends ScrollableFocusableDisplay{
+//	private final LineBorder FOCUS_LINE_BORDER = new LineBorder(GUIManager.FOCUSED_BORDER_COLOR);
+//	private final LineBorder UNFOCUS_LINE_BORDER = new LineBorder(GUIManager.BORDER_COLOR);
+//	private final FocusListener textDisplayFocusListener = new FocusListener(){
+//		public void focusGained(FocusEvent event) {
+//			setBorder(FOCUS_LINE_BORDER);
+//		 }
+//		public void focusLost(FocusEvent event){
+//			setBorder(UNFOCUS_LINE_BORDER);
+//		}
+//	};
 	
 	protected JTextPane textPane;
 	private int containerWidth;
@@ -34,8 +34,7 @@ public class TextDisplay extends JScrollPane{
 		containerHeight = height;
 		setSize(containerWidth, containerHeight);
 		setBorder(UNFOCUS_LINE_BORDER);
-		addFocusListener(textDisplayFocusListener);
-		
+		addFocusListener(displayFocusListener);		
 	}
 	private void createAndConfigureTextPane(){
 		textPane = new JTextPane();
