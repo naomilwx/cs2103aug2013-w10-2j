7c76818c275a99c34a1839afbb80d18049003982
Shuzhi <nusszyu@gmail.com>
2013-10-13 19:57:52 +0800

    refactoring the order of the methods in the CommandManager class

--- nailit/src/nailit/logic/command/CommandManager.java
+++ nailit/src/nailit/logic/command/CommandManager.java
@@ -45,6 +45,7 @@ public class CommandManager {
 		operationsHistory = new Vector<Command>();
 		currentTaskList = null;
 		filterContentForCurrentTaskList = null;
+		parserResultInstance = null;
 	}
 	
 	public Result executeCommand(ParserResult parserResultInstance)
@@ -56,7 +57,7 @@ public class CommandManager {
 
 	private Result doExecution() {
 		CommandType commandType = parserResultInstance.getCommand();
-		// in each command execute method like add(), we will create the 
+		// in each command execute method like add(), we will create the
 		// corresponding object and use them to execute the command.
 		// In addition, the command is added to operation History.
 		switch (commandType) {
@@ -64,28 +65,28 @@ public class CommandManager {
 			Result resultToReturn = add();
 			return resultToReturn;
 		}
-		case COMPLETE: {
-			Result resultToReturn = complete();
-			return resultToReturn;
-		}
 		case DELETE: {
 			Result resultToReturn = delete();
-			return resultToReturn;
-		}
-		case SEARCH: {
-			Result resultToReturn = search();
 			return resultToReturn;
 		}
 		case UPDATE: {
 			Result resultToReturn = update();
 			return resultToReturn;
 		}
-		case INVALID: {
-			break;
-		}
 		case DISPLAY: {
 			Result resultToReturn = display();
 			return resultToReturn;
+		}
+		case SEARCH: {
+			Result resultToReturn = search();
+			return resultToReturn;
+		}
+		case COMPLETE: {
+			Result resultToReturn = complete();
+			return resultToReturn;
+		}
+		case INVALID: {
+			break;
 		}
 		case EXIT: {
 			Result resultToReturn = exit();
@@ -97,69 +98,7 @@ public class CommandManager {
 		}
 		return null;
 	}
-
-	private Result exit() {
-		CommandExit newExitCommandObj = new CommandExit(parserResultInstance, storer);
-		Result resultToPassToGUI = newExitCommandObj.executeCommand();
-		return resultToPassToGUI;
-	}
-
-	private Result display() {
-		CommandDisplay newDisplayCommandObj = new CommandDisplay(parserResultInstance, storer, this, currentTaskList);
-		Result resultToPassToGUI = newDisplayCommandObj.executeCommand();
-		addNewCommandObjToOperationsHistory(newDisplayCommandObj);
-		return resultToPassToGUI;
-	}
-
-	private Result update() {
-		CommandUpdate newUpdateCommandObj = new CommandUpdate(parserResultInstance, storer, currentTaskList);
-		Result resultToPassToGUI = newUpdateCommandObj.executeCommand();
-		if(newUpdateCommandObj.updateSuccess()) {
-			int taskUpdatedDisplayID = newUpdateCommandObj.getDisplayID();
-			deleteTaskFromCurrentTaskList(taskUpdatedDisplayID);
-			addTaskToCurrentTaskList(resultToPassToGUI);
-		}
-		resultToPassToGUI.setTaskList(currentTaskList);
-		addNewCommandObjToOperationsHistory(newUpdateCommandObj);
-		return resultToPassToGUI;
-	}
-
-	private void addTaskToCurrentTaskList(Result result) {
-		Task taskUpdated = result.getTaskToDisplay();
-		currentTaskList.add(taskUpdated);
-	}
-
-	private void deleteTaskFromCurrentTaskList(int taskUpdatedDisplayID) {
-		currentTaskList.remove(taskUpdatedDisplayID - 1);
-	}
-
-	private Result search() {
-		return null;
-		// TODO Auto-generated method stub
-	}
-
-	private Result delete() {
-		CommandDelete newDeleteCommandObj = new CommandDelete(parserResultInstance, storer, currentTaskList);
-		Result resultToPassToGUI = newDeleteCommandObj.executeCommand();
-		
-		if(newDeleteCommandObj.deleteSuccess()) {
-			int taskDeletedDisplayID = newDeleteCommandObj.getTaskDisplayID();
-			deleteTheTaskInCurrentTaskList(taskDeletedDisplayID);
-		}
-		resultToPassToGUI.setTaskList(currentTaskList);
-		addNewCommandObjToOperationsHistory(newDeleteCommandObj);
-		return resultToPassToGUI;
-	}
-
-	private void deleteTheTaskInCurrentTaskList(int taskDeletedDisplayID) {
-		currentTaskList.remove(taskDeletedDisplayID - 1);
-	}
-
-	private Result complete() {
-		return null;
-		// TODO Auto-generated method stub
-	}
-
+	
 	private Result add() {
 		CommandAdd newAddCommandObj = new CommandAdd(parserResultInstance, storer); 
 		// the resultToPassToGUI does not have the currentTaskList
@@ -175,6 +114,68 @@ public class CommandManager {
 		// add the searchResult into the resultToPassToGUI
 		resultToPassToGUI.setTaskList(currentTaskList);
 		return resultToPassToGUI;
+	}
+	
+	private Result delete() {
+		CommandDelete newDeleteCommandObj = new CommandDelete(parserResultInstance, storer, currentTaskList);
+		Result resultToPassToGUI = newDeleteCommandObj.executeCommand();
+		
+		if(newDeleteCommandObj.deleteSuccess()) {
+			int taskDeletedDisplayID = newDeleteCommandObj.getTaskDisplayID();
+			deleteTheTaskInCurrentTaskList(taskDeletedDisplayID);
+		}
+		resultToPassToGUI.setTaskList(currentTaskList);
+		addNewCommandObjToOperationsHistory(newDeleteCommandObj);
+		return resultToPassToGUI;
+	}
+	
+	private Result update() {
+		CommandUpdate newUpdateCommandObj = new CommandUpdate(parserResultInstance, storer, currentTaskList);
+		Result resultToPassToGUI = newUpdateCommandObj.executeCommand();
+		if(newUpdateCommandObj.updateSuccess()) {
+			int taskUpdatedDisplayID = newUpdateCommandObj.getDisplayID();
+			deleteTaskFromCurrentTaskList(taskUpdatedDisplayID);
+			addTaskToCurrentTaskList(resultToPassToGUI);
+		}
+		resultToPassToGUI.setTaskList(currentTaskList);
+		addNewCommandObjToOperationsHistory(newUpdateCommandObj);
+		return resultToPassToGUI;
+	}
+	
+	private Result display() {
+		CommandDisplay newDisplayCommandObj = new CommandDisplay(parserResultInstance, storer, this, currentTaskList);
+		Result resultToPassToGUI = newDisplayCommandObj.executeCommand();
+		addNewCommandObjToOperationsHistory(newDisplayCommandObj);
+		return resultToPassToGUI;
+	}
+	
+	private Result search() {
+		return null;
+		// TODO Auto-generated method stub
+	}
+
+	private Result complete() {
+		return null;
+		// TODO Auto-generated method stub
+	}
+	
+	private Result exit() {
+		CommandExit newExitCommandObj = new CommandExit(parserResultInstance, storer);
+		Result resultToPassToGUI = newExitCommandObj.executeCommand();
+		return resultToPassToGUI;
+	}
+
+	private void addTaskToCurrentTaskList(Result result) {
+		Task taskUpdated = result.getTaskToDisplay();
+		currentTaskList.add(taskUpdated);
+	}
+
+	private void deleteTaskFromCurrentTaskList(int taskUpdatedDisplayID) {
+		currentTaskList.remove(taskUpdatedDisplayID - 1);
+	}
+
+	private void deleteTheTaskInCurrentTaskList(int taskDeletedDisplayID) {
+		currentTaskList.remove(taskDeletedDisplayID - 1);
 	}
 
 	private void addNewCommandObjToOperationsHistory(Command commandObjToAdd) {
