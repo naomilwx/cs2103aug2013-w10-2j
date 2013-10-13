4549e71e6545fbf4ff7dd633f03b7ca06fa0ec79
Naomi Leow <naomileowwx@gmail.com>
2013-10-14 01:51:18 +0800

    fixed display so display pane is shifted up when notification area is not visible

---
+++ nailit/src/nailit/gui/TextDisplay.java
@@ -0,0 +1,46 @@
+package nailit.gui;
+
+import java.awt.Dimension;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.border.LineBorder;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+
+public class TextDisplay extends ScrollableFocusableDisplay{
+	protected JTextPane textPane;
+	private int containerWidth;
+	private int containerHeight;
+	private int preferredHeight;
+	
+	public TextDisplay(int width, int height){
+		configureMainFrame(width, height);
+		createAndConfigureTextPane();
+	}
+	private void configureMainFrame(int width, int height){
+		containerWidth = width;
+		containerHeight = height;
+		setSize(containerWidth, containerHeight);
+		setBorder(UNFOCUS_LINE_BORDER);
+		addFocusListener(displayFocusListener);		
+	}
+	private void createAndConfigureTextPane(){
+		preferredHeight = containerWidth/2;
+		textPane = new JTextPane();
+		textPane.setEditable(false);
+		textPane.setPreferredSize(new Dimension(containerWidth, preferredHeight));
+		setViewportView(textPane);
+	}
+	protected void basicDisplay(String text){
+		centeriseTextDisplay();
+		textPane.setText(text);
+	}
+	protected void centeriseTextDisplay(){
+		SimpleAttributeSet textAttribute = new SimpleAttributeSet();
+		StyleConstants.setAlignment(textAttribute, StyleConstants.ALIGN_CENTER);
+		textPane.setParagraphAttributes(textAttribute, true);
+	}
+}
---
+++ nailit/src/nailit/logic/command/CommandManager.java
@@ -0,0 +1,247 @@
+package nailit.logic.command;
+
+import java.util.Collections;
+import java.util.Vector;
+
+import test.storage.StorageStub;
+
+import nailit.common.FilterObject;
+import nailit.common.Result;
+import nailit.common.Task;
+import nailit.logic.*;
+
+import nailit.storage.FileCorruptionException;
+import nailit.storage.StorageManager;
+import nailit.logic.CommandType;
+import nailit.logic.ParserResult;
+
+
+public class CommandManager {
+	// the storage object that the commandManager works with 
+	
+	// for testing
+	private StorageManager storer;
+//	private StorageManager storer;
+	
+	// the parserResult to use in the commandExcute
+	private ParserResult parserResultInstance;
+	
+	// the vector object is used to store the done operations
+	private Vector<Command> operationsHistory;
+	
+	// the current displaying task list in the GUI
+	private Vector<Task> currentTaskList;
+	
+	// the content of the filter that filters the currentTaskList
+	// eg. it can be "all", "CS2103, 2013-10-20, low"
+	private FilterObject filterContentForCurrentTaskList;
+	
+	// constructor
+	public CommandManager () throws FileCorruptionException 
+	{
+		//for testing
+		//storer = new StorageStub();
+		storer = new StorageManager();
+		operationsHistory = new Vector<Command>();
+		currentTaskList = new Vector<Task>();
+		filterContentForCurrentTaskList = new FilterObject();
+		parserResultInstance = null;
+	}
+	
+	public Result executeCommand(ParserResult parserResultInstance) throws Exception
+	{
+		if(parserResultInstance == null) {
+			throw new Exception("The parserResult Instance is a null object.");
+		} else {
+			this.parserResultInstance = parserResultInstance;
+			Result executedResult = doExecution();
+			return executedResult;
+		}
+	}
+
+	private Result doExecution() throws Exception {
+		CommandType commandType = parserResultInstance.getCommand();
+		// in each command execute method like add(), we will create the
+		// corresponding object and use them to execute the command.
+		// In addition, the command is added to operation History.
+		switch (commandType) {
+		case ADD: {
+			Result resultToReturn = add();
+			return resultToReturn;
+		}
+		case DELETE: {
+			Result resultToReturn = delete();
+			return resultToReturn;
+		}
+		case UPDATE: {
+			Result resultToReturn = update();
+			return resultToReturn;
+		}
+		case DISPLAY: {
+			Result resultToReturn = display();
+			return resultToReturn;
+		}
+		case SEARCH: {
+			Result resultToReturn = search();
+			return resultToReturn;
+		}
+		case COMPLETE: {
+			Result resultToReturn = complete();
+			return resultToReturn;
+		}
+		case INVALID: {
+			break;
+		}
+		case EXIT: {
+			Result resultToReturn = exit();
+			return resultToReturn;
+		}
+		default: {
+
+		}
+		}
+		return null;
+	}
+	
+	private Result add() {
+		CommandAdd newAddCommandObj = new CommandAdd(parserResultInstance, storer); 
+		// the resultToPassToGUI does not have the currentTaskList
+		Result resultToPassToGUI = newAddCommandObj.executeCommand();
+		addNewCommandObjToOperationsHistory(newAddCommandObj);
+		// the new added task may or may not should exist in the currentTaskList,
+		// check whether the added task fit the filterContentForCurrentTaskList
+		// if fit, add it and sort the current task list
+		if(isTheTaskFitTheFilter(resultToPassToGUI)) {
+			addTaskToCurrentTaskList(resultToPassToGUI);
+			sort();
+		}
+		// add the searchResult into the resultToPassToGUI
+		resultToPassToGUI.setTaskList(currentTaskList);
+		return resultToPassToGUI;
+	}
+	
+	private Result delete() throws Exception {
+
+		// delete the task according to its displayID in the taskList
+		CommandDelete newDeleteCommandObj = new CommandDelete(
+				parserResultInstance, storer, currentTaskList);
+		Result resultToPassToGUI = newDeleteCommandObj.executeCommand();
+		// if successfully deleted, update the currentTaskList by removing that
+		// task from the list
+		if (newDeleteCommandObj.deleteSuccess()) {
+			int taskDeletedDisplayID = newDeleteCommandObj.getTaskDisplayID();
+			deleteTheTaskInCurrentTaskList(taskDeletedDisplayID);
+		}
+		resultToPassToGUI.setTaskList(currentTaskList);
+		addNewCommandObjToOperationsHistory(newDeleteCommandObj);
+		return resultToPassToGUI;
+
+	}
+	
+	private Result update() throws Exception {
+		CommandUpdate newUpdateCommandObj = new CommandUpdate(parserResultInstance, storer, currentTaskList);
+		Result resultToPassToGUI = newUpdateCommandObj.executeCommand();
+		if(newUpdateCommandObj.updateSuccess()) {
+			int taskUpdatedDisplayID = newUpdateCommandObj.getDisplayID();
+			deleteTaskFromCurrentTaskList(taskUpdatedDisplayID);
+			addTaskToCurrentTaskList(resultToPassToGUI);
+		}
+		// the display ID for the task may change, so sort again
+		sort();
+		resultToPassToGUI.setTaskList(currentTaskList);
+		addNewCommandObjToOperationsHistory(newUpdateCommandObj);
+		return resultToPassToGUI;
+	}
+	
+	private Result display() throws Exception {
+		CommandDisplay newDisplayCommandObj = new CommandDisplay(parserResultInstance, storer, this);
+		Result resultToPassToGUI = newDisplayCommandObj.executeCommand();
+		addNewCommandObjToOperationsHistory(newDisplayCommandObj);
+		return resultToPassToGUI;
+	}
+	
+	private Result search() {
+		// if it is seach all, do not forget to set the filter.setIsSearchAll(true)
+		return null;
+		// TODO Auto-generated method stub
+	}
+
+	private Result complete() {
+		return null;
+		// TODO Auto-generated method stub
+	}
+	
+	private Result exit() {
+		CommandExit newExitCommandObj = new CommandExit(parserResultInstance, storer);
+		Result resultToPassToGUI = newExitCommandObj.executeCommand();
+		return resultToPassToGUI;
+	}
+
+	private void addTaskToCurrentTaskList(Result result) {
+		Task taskUpdated = result.getTaskToDisplay();
+		currentTaskList.add(taskUpdated);
+	}
+
+	private void deleteTaskFromCurrentTaskList(int taskUpdatedDisplayID) {
+		currentTaskList.remove(taskUpdatedDisplayID - 1);
+	}
+
+	private void deleteTheTaskInCurrentTaskList(int taskDeletedDisplayID) {
+		currentTaskList.remove(taskDeletedDisplayID - 1);
+	}
+
+	private void addNewCommandObjToOperationsHistory(Command commandObjToAdd) {
+		operationsHistory.add(commandObjToAdd);
+	}
+	
+	public Vector<Command> getOperationsHistory() {
+		return operationsHistory;
+	}
+	
+	// sort the Tasks in the currentTaskList according to isCompleted, date, priority
+	private void sort() {
+		ComparatorForTwoTaskObj newComparator = new ComparatorForTwoTaskObj();
+		Collections.sort(currentTaskList, newComparator);
+	}
+	
+	private boolean isTheTaskFitTheFilter(Result resultToPassToGUI) {
+		if(filterContentForCurrentTaskList == null) {
+			return false;
+		} else {
+			Task taskToCompare = resultToPassToGUI.getTaskToDisplay();
+			if(filterContentForCurrentTaskList.getIsSearchAll()) {
+				return true;
+			} else if(taskToCompare.getName() == filterContentForCurrentTaskList.getName()) {
+				return true;
+			} else if(taskToCompare.getStartTime().equals(filterContentForCurrentTaskList.getStartTime())) {
+				return true;
+			} else if(taskToCompare.getEndTime().equals(filterContentForCurrentTaskList.getEndTime())) {
+				return true;
+			} else if(taskToCompare.getPriority().equals(filterContentForCurrentTaskList.getPriority())) {
+				return true;
+			} else if(taskToCompare.getTag().equals(filterContentForCurrentTaskList.getTag())) {
+				return true;
+			} else if(taskToCompare.checkCompleted() == filterContentForCurrentTaskList.isCompleted()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public Vector<Task> getCurrentTaskList() {
+		return currentTaskList;
+	}
+	
+	public FilterObject getCurrentFilterObj() {
+		return filterContentForCurrentTaskList;
+	}
+
+	public void setCurrentList(Vector<Task> vectorOfTasks) {
+		currentTaskList = vectorOfTasks;
+	}
+
+	public void setCurrentFilterSearchAll() {
+		filterContentForCurrentTaskList.setIsSearchAll(true);
+	}
+}
