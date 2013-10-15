bf308bba25fb844f42279d26ebc420ff063f5a41
Naomi Leow <naomileowwx@gmail.com>
2013-10-15 18:11:15 +0800

    addeded code in applauncher to allow logging

--- nailit/src/nailit/AppLauncher.java
+++ nailit/src/nailit/AppLauncher.java
@@ -1,14 +1,42 @@
 package nailit;
 
 import java.awt.EventQueue;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
 
 import nailit.gui.GUIManager;
 
 public class AppLauncher {
+	private static final String LOG_FILE = "naillog.txt";
+	private static final String START_LOGGING_MSG = "Start up";
+	private static final String END_LOGGING_MSG = "Shut down";
+	
+	private static Logger appLog = Logger.getLogger(AppLauncher.class.getName());
 	GUIManager GUI;
+	
 	public static void main(String[] args){
 		AppLauncher launcher = new AppLauncher();
+		launcher.startAppLogger();
+		appLog.info(START_LOGGING_MSG);
 		launcher.run();
+	}
+	private void startAppLogger(){
+		try {
+			FileHandler logFileHandler = new FileHandler(LOG_FILE);
+			appLog.addHandler(logFileHandler);
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void logMessage(Level lvl, String msg){
+		appLog.log(lvl,msg);
 	}
 	
 	public void run(){
@@ -29,6 +57,7 @@ public class AppLauncher {
 		GUI.setFocusOnCommandBar();
 	}
 	public void exit(){
+		appLog.info(END_LOGGING_MSG);
 		System.exit(0);
 	}
 }
