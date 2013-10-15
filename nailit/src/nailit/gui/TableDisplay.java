974f6831ac54e6acd64cfb1fca01bce4c7834707
Naomi Leow <naomileowwx@gmail.com>
2013-10-15 16:56:06 +0800

    increased header size

--- nailit/src/nailit/gui/TableDisplay.java
+++ nailit/src/nailit/gui/TableDisplay.java
@@ -1,6 +1,7 @@
 package nailit.gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyAdapter;
@@ -15,6 +16,7 @@ import javax.swing.JTable;
 import javax.swing.JTextPane;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
@@ -27,7 +29,7 @@ import nailit.gui.renderer.TaskNameDisplayRenderer;
 import nailit.gui.renderer.IDDisplayRenderer;
 
 public class TableDisplay extends ScrollableFocusableDisplay{
-	private static final int TABLE_HEADER_HEIGHT = 50;
+	private static final int TABLE_HEADER_HEIGHT = 40;
 	private static final int TABLE_ROW_HEIGHT = 50;
 	private static final int NO_SELECTED_ROW = -1;
 	
@@ -103,7 +105,7 @@ public class TableDisplay extends ScrollableFocusableDisplay{
 	}
 	private void createAndConfigureTable() {
 		initialiseTableStructures();
-		setHeaderRenderer();
+		configureTableHeader();
 		setHeaderText();
 		configureTable();
 	}
@@ -168,8 +170,10 @@ public class TableDisplay extends ScrollableFocusableDisplay{
 		noOfCols = tableHeaderLabel.size();
 		tableModel.setDataVector(tableRows, tableHeaderLabel);
 	}
-	private void setHeaderRenderer(){
-		table.getTableHeader().setDefaultRenderer(new TableHeaderRenderer());
+	private void configureTableHeader(){
+		JTableHeader header = table.getTableHeader();
+		header.setDefaultRenderer(new TableHeaderRenderer());
+		header.setPreferredSize(new Dimension(containerWidth, TABLE_HEADER_HEIGHT));;
 	}
 	private void setRowWidths(){
 		switch(tableDisplayType){
