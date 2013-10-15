a81d45823722048b9d9cc04fc6008f1f4b5405ca
Naomi Leow <naomileowwx@gmail.com>
2013-10-15 10:30:12 +0800

    with new table cell renderer

--- nailit/src/nailit/gui/renderer/TaskNameDisplayRenderer.java
+++ nailit/src/nailit/gui/renderer/TaskNameDisplayRenderer.java
@@ -6,13 +6,15 @@ public class TaskNameDisplayRenderer extends DefaultTableCellRenderer{
 	private static final String cellStyle = "<head><style type = \"text/css\">" 
 			+ "p.name {font-size: 15px;}"
 			+ "</style></head>";
+	private static final String TASK_DONE_CODE = "&#9745";
+	private static final String TASK_NOT_DONE_CODE = "&#9744 ";
 	@Override
 	protected void setValue(Object value){
 		String output = "<html>" + cellStyle + (String) value + "</html>";
 		setText(output);
 	}
 	public static String formatTaskNameCellDisplay(String taskName, String tag){
-		String formattedString = "<p class = \"name\">" + taskName + "</p>";
+		String formattedString = "<p class = \"name\">"+ taskName + "</p>";
 		if(!tag.isEmpty()){
 			formattedString = formattedString + "<p class = \"tag\">" + tag + "</p>";
 		}
