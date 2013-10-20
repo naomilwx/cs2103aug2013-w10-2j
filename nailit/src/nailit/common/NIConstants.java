ade41979968fe1ffcc044e5b1d62c7930a3c89cf
Naomi Leow <naomileowwx@gmail.com>
2013-10-21 00:32:04 +0800

    implemented set existing task description as text in command bar on ctrl-enter

---
+++ nailit/src/nailit/common/NIConstants.java
@@ -0,0 +1,11 @@
+package nailit.common;
+/**************************
+ * This class is used to store all the constants.
+ ************************* */
+public class NIConstants {
+	public static final String NORMAL_FIELD_SPLITTER = ",";
+	public static final String UPDATE_FIELD_SPLITTER = " ";
+	public static final String DISPLAY_DATE_FORMAT = "dd MMM YYYY";
+	public static final String DISPLAY_TIME_FORMAT = "h:mma";
+	public static final String DISPLAY_FULL_DATETIME_FORMAT = "dd MMM YYYY h:mma";
+}
---
+++ nailit/src/nailit/logic/command/Command.java
@@ -0,0 +1,26 @@
+package nailit.logic.command;
+
+import test.storage.StorageManagerStub;
+import nailit.common.Result;
+import nailit.logic.ParserResult;
+import nailit.storage.StorageManager;
+
+public abstract class Command {
+
+	protected ParserResult parserResultInstance;
+	
+	protected StorageManager storer;
+	
+	private Result executedResult;
+	// constructor
+	public Command(ParserResult resultInstance, StorageManager storerToUse) {
+		parserResultInstance =  resultInstance;
+		this.storer = storerToUse;
+	}
+
+
+	public abstract Result executeCommand() throws Exception;
+	
+	public abstract int getTaskID(); 
+	
+}
