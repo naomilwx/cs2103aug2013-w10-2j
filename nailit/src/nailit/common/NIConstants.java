8cd3b1109dac81340dfb32c4363e474df1645a9c
Wei Wenbo <weiwenbo3@yahoo.com.sg>
2013-10-21 20:17:52 +0800

    add new splitters for the line splitter in description

--- nailit/src/nailit/common/NIConstants.java
+++ nailit/src/nailit/common/NIConstants.java
@@ -5,6 +5,7 @@ package nailit.common;
 public class NIConstants {
 	public static final String NORMAL_FIELD_SPLITTER = ",";
 	public static final String UPDATE_FIELD_SPLITTER = " ";
+	public static final String DESCRIPTION_LINE_SPLITER = "  ´´¨¨";
 	public static final String DISPLAY_DATE_FORMAT = "dd MMM YYYY";
 	public static final String DISPLAY_TIME_FORMAT = "h:mma";
 	public static final String DISPLAY_FULL_DATETIME_FORMAT = "dd MMM YYYY h:mma";
