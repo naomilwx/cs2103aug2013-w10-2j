d33ece66755a7d2c55bc367f0ef2c5b2f6253864
Naomi Leow <naomileowwx@gmail.com>
2013-10-23 04:02:22 +0800

    removed task ID from add/delete notifications

--- nailit/src/nailit/logic/command/CommandDelete.java
+++ nailit/src/nailit/logic/command/CommandDelete.java
@@ -28,7 +28,7 @@ public class CommandDelete extends Command{
 	private boolean isUndoSuccess;
 	private boolean isRedoSuccess;
 
-	private static final String SUCCESS_MSG = "Task: [ID %1s]has been successfully deleted.";
+	private static final String SUCCESS_MSG = "Task: %1s has been successfully deleted.";
 	private static final String FEEDBACK_FOR_NOT_EXISTING_TASK = "Task [ID %1d] not found. Cannot delete non-existant task.";
 	private static final String FEEDBACK_FOR_NOT_EXISTING_TASK_IN_TASK_LIST = "Task [ID %1d] does not exist in the current task list. Cannot delete non-existant task."; 
 	private static final String EXCEPTION_MESSAGE_FOR_DISPLAY_ID_IS_NULL = "Display ID in the parserResult instance is null."; 
@@ -111,7 +111,7 @@ public class CommandDelete extends Command{
 	}
 
 	private void createResultObject() {
-		String notificationStr = String.format(SUCCESS_MSG, taskToDeleteDisplayID);
+		String notificationStr = String.format(SUCCESS_MSG, taskToRemove.getName());
 		executedResult = new Result(false, true, Result.EXECUTION_RESULT_DISPLAY, notificationStr, taskToRemove, null, null);	
 		executedResult.setDeleteStatus(true);
 	}
