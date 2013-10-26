09c391a4cf415c6861aee09ccbb97bb3800a932d
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-10-26 14:23:35 +0800

    add some files

--- nailit/src/nailit/logic/CommandType.java
+++ nailit/src/nailit/logic/CommandType.java
@@ -5,7 +5,12 @@ import nailit.common.TaskPriority;
 public enum CommandType {
 	ADD, COMPLETE, DELETE, DISPLAY, REDO, SEARCH, SHOWHISTORY, UNCOMPLETE, UNDO, UPDATE, EXIT, INVALID;
 
-
+	public static final int DELETE_COST = 1;
+	public static final int INSERT_COST = 1;
+	public static final int REPLACE_COST = 2;
+	public static final int TWIDDLE_COST = 2;
+	
+	
 	public static boolean isCommandType(String p){
 		for(CommandType type: CommandType.values()){
 			if(p.equalsIgnoreCase(type.toString())){
@@ -15,12 +20,20 @@ public enum CommandType {
 		return false;
 	}
 	
-	public static boolean isApproximateCommandType(String p){
+	public static int calculateDistance(String a, String b){
+		int answer=0;
+		
+		return answer;
+	}
+	
+	public static CommandType isApproximateCommandType(String p){
+		int min = Integer.MAX_VALUE;
+		CommandType answer = CommandType.INVALID;
 		for(CommandType type: CommandType.values()){
-			if(p,type.toString())){
-				return true;
+			if (calculateDistance(p,type.toString()) > min){
+				answer = type;
 			}
 		}
-		return false;
+		return answer;
 	}
 }
