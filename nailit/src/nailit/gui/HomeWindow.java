e0d0a43b0bb034c9b8cc9b59d47745050bb5ea97
Shuzhi <nusszyu@gmail.com>
2013-10-26 23:09:20 +0800

    add undo and redo to addReminder

---
+++ nailit/src/nailit/gui/HomeWindow.java
@@ -0,0 +1,15 @@
+//@author A0091372H
+package nailit.gui;
+
+import javax.swing.JPanel;
+
+public class HomeWindow extends ExtendedWindow{
+	public HomeWindow(GUIManager GUIMain, int width) {
+		super(GUIMain, width);
+		initialiseAndConfigureWindowContent();
+	}
+	
+	protected void displayReminders(){
+		//TODO:
+	}
+}
---
+++ nailit/src/nailit/logic/command/ComparatorForTwoTaskObj.java
@@ -0,0 +1,81 @@
+package nailit.logic.command;
+
+import java.util.Comparator;
+
+import org.joda.time.DateTime;
+
+import nailit.common.Task;
+import nailit.common.TaskPriority;
+
+public class ComparatorForTwoTaskObj implements Comparator<Task>{
+	@Override
+	public int compare(Task hostTask, Task taskToCompare) {
+		boolean hostTaskIsCompleted = hostTask.checkCompleted();
+		boolean taskToCompareIsCompleted = taskToCompare.checkCompleted();
+		
+		int hostTaskPriorityCode = hostTask.getPriority().getPriorityCode();
+		int taskToComparePriorityCode = taskToCompare.getPriority().getPriorityCode();
+		
+		DateTime hostTaskDate = hostTask.getStartTime();
+		DateTime taskToCompareDate = taskToCompare.getStartTime();
+		
+		String hostTaskName = hostTask.getName();
+		String taskToCompareName = taskToCompare.getName();
+		if(compareCompleteLevel(hostTaskIsCompleted, taskToCompareIsCompleted) == 0) {
+			if(compareDate(hostTaskDate, taskToCompareDate) == 0) {
+				if(comparePriorityCode(hostTaskPriorityCode, taskToComparePriorityCode) == 0){
+					return compareName(hostTaskName, taskToCompareName);
+				}else{
+					return comparePriorityCode(hostTaskPriorityCode, taskToComparePriorityCode);
+				}
+			} else { // compare Date
+				return compareDate(hostTaskDate, taskToCompareDate);
+			}
+		} else { // not same completeLevel
+			return compareCompleteLevel(hostTaskIsCompleted, taskToCompareIsCompleted);
+		}
+	}
+
+	private int compareDate(DateTime hostTaskDate, DateTime taskToCompareDate) {
+		// has start date is better than null
+		if(hostTaskDate != null && taskToCompareDate == null) {
+			return -1;
+		} else if(hostTaskDate == null && taskToCompareDate != null) { 
+			return 1;
+		} else if(hostTaskDate == null && taskToCompareDate == null) {
+			return 0;
+		} else {
+			return hostTaskDate.compareTo(taskToCompareDate); 
+		}
+	}
+
+	private int comparePriorityCode(int hostTaskPriorityCode,
+			int taskToComparePriorityCode) {
+		if(hostTaskPriorityCode < taskToComparePriorityCode) {
+			return 1;
+		} else if(hostTaskPriorityCode > taskToComparePriorityCode) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+	
+	private int compareName(String hostTaskName, String taskToCompareName){
+		assert hostTaskName != null;
+		assert taskToCompareName != null;
+		return hostTaskName.compareTo(taskToCompareName);
+	}
+
+	private int compareCompleteLevel(boolean hostTaskIsCompleted,
+			boolean taskToCompareIsCompleted) {
+		if(hostTaskIsCompleted == taskToCompareIsCompleted) {
+			return 0;
+		} else if(hostTaskIsCompleted == false && taskToCompareIsCompleted == true) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
+
+}
