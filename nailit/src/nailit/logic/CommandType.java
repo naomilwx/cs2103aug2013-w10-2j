a1931a6eb2e312744c914ba63fe2214ed6ddccd5
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-10-26 16:05:55 +0800

    improve updateParser: let it set as null

---
+++ nailit/src/nailit/logic/CommandType.java
@@ -0,0 +1,38 @@
+package nailit.logic;
+
+import nailit.common.TaskPriority;
+import org.apache.commons.lang3.StringUtils;
+
+public enum CommandType {
+	ADD, ADDREMINDER, COMPLETE, DELETE, DISPLAY, REDO, SEARCH, SHOWHISTORY, UNCOMPLETE, UNDO, UPDATE, EXIT, INVALID;
+
+	public static final int DELETE_COST = 1;
+	public static final int INSERT_COST = 1;
+	public static final int REPLACE_COST = 2;
+	public static final int TWIDDLE_COST = 2;
+	
+	
+	public static boolean isCommandType(String p){
+		for(CommandType type: CommandType.values()){
+			if(p.equalsIgnoreCase(type.toString())){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public static CommandType isApproximateCommandType(String p){
+		int min = Integer.MAX_VALUE;
+		CommandType answer = CommandType.INVALID;
+		for(CommandType type: CommandType.values()){
+			if (StringUtils.getLevenshteinDistance(type.values().toString(), p) < min){
+				min = StringUtils.getLevenshteinDistance(type.values().toString(), p);
+				if (min<p.length()){
+					answer = type;
+				}
+			}
+		}
+		return answer;
+	}
+}
