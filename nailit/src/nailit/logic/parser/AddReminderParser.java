f19f0963b69f8a5ddd5522c2edf5300ce149ee3e
Naomi Leow <naomileowwx@gmail.com>
2013-10-29 13:11:02 +0800

    modified formatting of reminders, width of window

---
+++ nailit/src/nailit/logic/parser/AddReminderParser.java
@@ -0,0 +1,47 @@
+package nailit.logic.parser;
+
+import nailit.common.NIConstants;
+import nailit.common.TaskPriority;
+import nailit.logic.CommandType;
+import nailit.logic.ParserResult;
+
+public class AddReminderParser extends Parser{
+
+	private String userCommand;
+	private String[] listOfCommand;
+	
+	public AddReminderParser (String command){
+		userCommand = command;
+	}
+	
+	@Override
+	public ParserResult execute(){
+		ParserResult resultExecution = new ParserResult();
+		resultExecution.setCommand(CommandType.ADDREMINDER);
+		listOfCommand = userCommand.split(NIConstants.NORMAL_FIELD_SPLITTER);
+		for (int i=0; i<listOfCommand.length; i++) {
+			listOfCommand[i] = listOfCommand[i].trim();
+			if (Parser.isNumber(listOfCommand[i])){
+				resultExecution.setTaskID(Integer.parseInt(listOfCommand[i]));
+			}else if (Parser.isDateTime(listOfCommand[i])) {
+				if (resultExecution.getEndTime() == null) {
+					if (Parser.numberOfTime(listOfCommand[i]) == 2) {
+						resultExecution.setStartTime(Parser.retrieveDateTimeFirst(listOfCommand[i]));
+						resultExecution.setEndTime(Parser.retrieveDateTimeSecond(listOfCommand[i]));
+					} else {
+						resultExecution.setEndTime(Parser.retrieveDateTime(listOfCommand[i]));
+					}
+				} else { 
+					resultExecution.setEndTime(Parser.retrieveDateTime(listOfCommand[i]));
+				}
+			} 
+		}
+		
+		if (!resultExecution.isNullStartTime() && resultExecution.isNullEndTime()){
+			resultExecution.setEndTime(resultExecution.getStartTime());
+			resultExecution.setStartTime(null);
+		}
+		return resultExecution;
+	}
+
+}
