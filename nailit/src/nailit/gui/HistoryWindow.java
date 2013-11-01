570a5618d5061ed1dc737b822f13bf3beb9e83de
Naomi Leow <naomileowwx@gmail.com>
2013-11-01 18:59:54 +0800

    changed history window to normal text display

--- nailit/src/nailit/gui/HistoryWindow.java
+++ nailit/src/nailit/gui/HistoryWindow.java
@@ -9,24 +9,10 @@ public class HistoryWindow extends ExtendedWindow{
 	public HistoryWindow(GUIManager GUIMain, int width) {
 		super(GUIMain, width);
 	}
-	@Override
-	protected void initialiseAndConfigureWindowContent(){
-		setWindowContentSize();
-		displayPane = new TableDisplay(contentWidth, contentHeight, Result.HISTORY_DISPLAY);
-		displayPane.setLocation(EXTENDED_WINDOW_X_BUFFER, EXTENDED_WINDOW_Y_BUFFER);
-		addItem(displayPane);
-	}
+	
 	protected void displayHistoryList(Vector<String> list){
 		//TODO
-		Vector<String> row;
-		int rowNum;
-		for(int i = 0; i < list.size(); i++){
-			row = new Vector<String>();
-			rowNum = i+1;
-			row.add(""+rowNum);
-			row.add(list.get(i));
-			((TableDisplay) displayPane).addContentToTable(row);
-		}
+		
 	}
 	
 }
