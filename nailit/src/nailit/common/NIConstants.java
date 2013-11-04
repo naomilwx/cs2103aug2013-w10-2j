fdcca2baed9dbe77aa12683f35ff6703cf188c99
Naomi Leow <naomileowwx@gmail.com>
2013-11-04 21:34:14 +0800

    added new constants

--- nailit/src/nailit/common/NIConstants.java
+++ nailit/src/nailit/common/NIConstants.java
@@ -6,9 +6,11 @@ public class NIConstants {
 	public static final String HARDDISK_FIELD_SPLITTER = "$%";
 	public static final String NORMAL_FIELD_SPLITTER = ",";
 	public static final String UPDATE_FIELD_SPLITTER = " ";
-	public static final String DESCRIPTION_LINE_SPLITER = "  ´´¨¨";
+	public static final String DESCRIPTION_LINE_SPLITER = "*&*";
 	public static final String EMPTY_STRING = "";
 	public static final String DISPLAY_DATE_FORMAT = "d MMM YYYY";
 	public static final String DISPLAY_TIME_FORMAT = "h:mma";
 	public static final String DISPLAY_FULL_DATETIME_FORMAT = "dd MMM YYYY h:mma";
+	public static final int HISTORY_UNDO_INDEX = 0;
+	public static final in HISTORY_REDO_INDEX = 1;
 }
