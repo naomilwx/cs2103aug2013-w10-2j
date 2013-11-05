74dd5f6dc0993795b8c83bbe615a2f0116f295c4
Naomi Leow <naomileowwx@gmail.com>
2013-11-05 10:03:20 +0800

    reposition help window

--- nailit/src/nailit/gui/HistoryWindow.java
+++ nailit/src/nailit/gui/HistoryWindow.java
@@ -16,30 +16,34 @@ public class HistoryWindow extends ExtendedWindow{
 	}
 	
 	protected void displayHistoryList(Vector<Vector <String>> list){
-		//TODO
 		if(list == null){
 			return;
 		}
 		StringBuilder str = new StringBuilder();
 		str.append("<html>");
+		
+		Vector<String> undidCommands = list.get(NIConstants.HISTORY_UNDO_INDEX);
 		str.append(COMMANDS_EXECUTED_HEADER);
 		str.append("<table>");
-		Vector<String> undidCommands = list.get(NIConstants.HISTORY_UNDO_INDEX);
 		for(String command: undidCommands){
 			str.append("<tr>");
 			str.append(command);
 			str.append("</tr>");
 		}
 		str.append("</table>");
+		
 		Vector<String> redoableCommands = list.get(NIConstants.HISTORY_REDO_INDEX);
-		str.append(COMMANDS_UNDID_HEADER);
-		str.append("<table>");
-		for(String command: redoableCommands){
-			str.append("<tr>");
-			str.append(command);
-			str.append("</tr>");
+		if(!redoableCommands.isEmpty()){
+			str.append(COMMANDS_UNDID_HEADER);
+			str.append("<table>");
+			for(String command: redoableCommands){
+				str.append("<tr>");
+				str.append(command);
+				str.append("</tr>");
+			}
+			str.append("</table>");
 		}
-		str.append("</table>");
+		
 		str.append("</html>");
 		((TextDisplay) displayPane).displayHTMLFormattedText(str.toString());
 	}
