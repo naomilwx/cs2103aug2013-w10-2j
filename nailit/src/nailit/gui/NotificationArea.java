08d885528ca55d4f6ea1b1f6207d3ac5000d4dc0
Naomi Leow <naomileowwx@gmail.com>
2013-11-09 15:27:08 +0800

    changed commandbar text

--- nailit/src/nailit/gui/NotificationArea.java
+++ nailit/src/nailit/gui/NotificationArea.java
@@ -12,7 +12,7 @@ public class NotificationArea extends TextDisplay{
 	private static final int NOTIFICATION_BORDER_WIDTH = 3;
 	private static final Color SUCCESS_COLOR = new Color(204, 240, 153);
 	private static final Color FAILURE_COLOR = new Color(250, 88, 88);
-	protected static final int NOTIFICATION_HEIGHT = 40;
+	protected static final int NOTIFICATION_HEIGHT = 45;
 	
 	public NotificationArea(int width){
 		super(width, NOTIFICATION_HEIGHT);
