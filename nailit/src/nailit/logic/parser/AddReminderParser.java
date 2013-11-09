7aeb360a97b7668466da31f153bceee5eec3f053
Naomi Leow <naomileowwx@gmail.com>
2013-11-09 18:28:12 +0800

    changed font for text display

---
+++ nailit/src/nailit/logic/parser/AddReminderParser.java
@@ -0,0 +1,34 @@
+package nailit.logic.parser;
+
+//@author A0105559B
+import nailit.common.NIConstants;
+import nailit.logic.CommandType;
+import nailit.logic.ParserResult;
+
+public class AddReminderParser extends Parser{
+
+	private String userCommand;
+	private String[] listOfCommands;
+	
+	public AddReminderParser (String command){
+		userCommand = command;
+	}
+	
+	@Override
+	public ParserResult execute(){
+		ParserResult resultExecution = new ParserResult();
+		resultExecution.setCommand(CommandType.ADDREMINDER);
+		listOfCommands = userCommand.split(NIConstants.NORMAL_FIELD_SPLITTER);
+		for (int i=0; i<listOfCommands.length; i++) {
+			listOfCommands[i] = listOfCommands[i].trim();
+			if (Parser.isNumber(listOfCommands[i])){
+				resultExecution.setTaskId(Integer.parseInt(listOfCommands[i]));
+				System.out.println(listOfCommands[i]);
+			}else if (Parser.isDateTime(listOfCommands[i])) {
+				resultExecution.setReminderTime(Parser.retrieveDateTime(listOfCommands[i]));
+			} 
+		}
+		return resultExecution;
+	}
+
+}
---
+++ nailit/src/test/overall/OverAllTestDelete.java
@@ -0,0 +1,54 @@
+//@author A0091372H
+package test.overall;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import test.common.TaskStub;
+import nailit.common.NIConstants;
+import nailit.common.Task;
+import nailit.logic.CommandType;
+import nailit.logic.LogicManager;
+
+public class OverAllTestDelete {
+	Random gen = new Random();
+	LogicManager logic = OverallTestAdd.logic;
+	int overallTaskNum = OverallTestAdd.count;
+	@Test
+	public void runTest(){
+		deleteTest();
+		addAfterDelelete();
+	}
+	public void deleteTest(){
+		int taskID = gen.nextInt(overallTaskNum) + 1;
+		try {
+			logic.executeCommand(CommandType.DISPLAY + " all");
+			Task task = logic.executeDirectIDCommand(CommandType.DISPLAY, taskID).getTaskToDisplay();
+			Task deletedTask = logic.executeCommand("delete " + taskID).getTaskToDisplay();
+			OverallTestSuite.compareTasksAttributes(task, deletedTask);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void addAfterDelelete(){
+		Task expectedTask;
+		try {
+			expectedTask = new TaskStub(TaskStub.GENERATE_TASK);
+			expectedTask.setID(overallTaskNum + 1);
+			String commandString = "add "
+					+expectedTask.getEndTime().toString(NIConstants.DISPLAY_FULL_DATETIME_FORMAT)+ ","
+					+expectedTask.getPriority() + "," 
+					+expectedTask.getTag() + ","
+					+expectedTask.getName();
+			Task addedTask = logic.executeCommand(commandString).getTaskToDisplay();
+			OverallTestSuite.compareTasksAttributes(expectedTask, addedTask);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
