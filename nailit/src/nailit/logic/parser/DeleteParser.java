f8cc403dfe8d85df3a184bf60a7ff4bc504470ef
Naomi Leow <naomileowwx@gmail.com>
2013-11-10 15:54:51 +0800

    scroll notification display to top

---
+++ nailit/src/nailit/logic/parser/DeleteParser.java
@@ -0,0 +1,33 @@
+package nailit.logic.parser;
+
+//@author A0105559B
+import nailit.logic.CommandType;
+import nailit.logic.ParserResult;
+import nailit.logic.exception.InvalidCommandFormatException;
+
+public class DeleteParser extends Parser {
+	
+	private String userCommand;
+	
+	public DeleteParser (String command){
+		userCommand = command;
+	}
+	
+	@Override
+	public ParserResult execute() throws InvalidCommandFormatException{
+		ParserResult resultExecution = new ParserResult();
+	
+		resultExecution.setCommand(CommandType.DELETE);
+		if (userCommand.equals("")){
+			throw new InvalidCommandFormatException(CommandType.DELETE,"Wrong Format: Cannot delete an empth task, please specify your task name");
+		}
+		
+		if (Parser.isNumber(userCommand)){
+			resultExecution.setTaskId(Integer.parseInt(userCommand));
+		}else{ 
+			throw new InvalidCommandFormatException(CommandType.DELETE,"Wrong Format: the ID should be specified");
+		}
+		
+		return resultExecution;
+	}
+}
---
+++ nailit/src/nailit/storage/FileCorruptionException.java
@@ -0,0 +1,9 @@
+package nailit.storage;
+/**
+ * @author a0105683e
+ * */
+public class FileCorruptionException extends Exception{
+	public FileCorruptionException(String message){
+		super(message);
+	}
+}
