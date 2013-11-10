311c5550e1645871de15292c41189c13a18f01e4
Naomi Leow <naomileowwx@gmail.com>
2013-11-10 13:03:54 +0800

    print error messages given by parser

---
+++ nailit/src/nailit/logic/parser/UncompleteParser.java
@@ -0,0 +1,27 @@
+package nailit.logic.parser;
+
+//@author A0105559B
+import nailit.logic.CommandType;
+import nailit.logic.ParserResult;
+
+public class UncompleteParser extends Parser{
+
+private String userCommand;
+	
+	public UncompleteParser (String command){
+		userCommand = command;
+	}
+	
+	@Override
+	public ParserResult execute(){
+		ParserResult resultExecution = new ParserResult();
+		resultExecution.setCommand(CommandType.UNCOMPLETE);
+		if (Parser.isNumber(userCommand)){
+			resultExecution.setTaskId(Integer.parseInt(userCommand));
+		}else{ 
+			throw new Error("Wrong Format");
+		}
+		return resultExecution;
+	}
+
+}
