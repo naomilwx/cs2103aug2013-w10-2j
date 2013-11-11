a54b6f0890f20ee521bc06d8aa11122e4100fb34
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-11-11 22:42:29 +0800

    finish update parser

--- nailit/src/test/logic/parser/UpdateParserTest.java
+++ nailit/src/test/logic/parser/UpdateParserTest.java
@@ -1,11 +1,13 @@
 package test.logic.parser;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import test.logic.command.CommandTest;
 import static org.junit.Assert.*;
 import nailit.common.CommandType;
 import nailit.common.NIConstants;
+import nailit.common.TaskPriority;
 import nailit.logic.exception.InvalidCommandFormatException;
 import nailit.logic.parser.UpdateParser;
 import nailit.logic.ParserResult;
@@ -16,14 +18,59 @@ public class UpdateParserTest {
 	public void test() throws InvalidCommandFormatException{
 		ParserResult expectedUpdate1 = new ParserResult();
 		
-//		expectedUpdate1.setTaskID(12);
+		// Update name field
+		expectedUpdate1.setTaskId(12);
 		expectedUpdate1.setCommand(CommandType.UPDATE);
+		expectedUpdate1.setName("CS Assignment 2");
 		
-		testExecuteName("CS Assignment 2","12 Name CS Assignment 2");
-		testExecuteTag("#Work#", "12 Tag #Work#");
-		testExecuteTaskPriority("LOW", "12 Priority LOW");
-		testExecuteStartTime("11 Feb 2013", "12 Start feb 11 2013");
-		testExecuteEndTime("11 Feb 2013", "12 end feb 11 2013");
+		testExecuteName(expectedUpdate1.getName(),"12, Name,CS Assignment 2");
+		testExecuteCommandType(expectedUpdate1.getCommand().toString(),"12, Name,CS Assignment 2");
+		testExecute(expectedUpdate1.getTaskID(), "12, Name, CS Assignment 2");
+		
+		// Update tag field
+		ParserResult expectedUpdate2 = new ParserResult();
+		
+		expectedUpdate2.setTaskId(12);
+		expectedUpdate2.setCommand(CommandType.UPDATE);
+		expectedUpdate2.setTag("#Work#");
+		
+		testExecuteTag(expectedUpdate2.getTag(),"12,Tag,#Work#");
+		testExecuteCommandType(expectedUpdate2.getCommand().toString(),"12,Tag,#Work#");
+		testExecute(expectedUpdate2.getTaskID(), "12,Tag,#Work#");
+		
+		// Update priority field
+		ParserResult expectedUpdate3 = new ParserResult();
+				
+		expectedUpdate3.setTaskId(12);
+		expectedUpdate3.setCommand(CommandType.UPDATE);
+		expectedUpdate3.setPriority(TaskPriority.LOW);
+				
+		testExecuteTaskPriority(expectedUpdate3.getPriority().toString(),"12,Priority,LOW");
+		testExecuteCommandType(expectedUpdate3.getCommand().toString(),"12,Priority,LOW");
+		testExecute(expectedUpdate3.getTaskID(), "12,Priority,LOW");
+		
+		// Update start time field
+		ParserResult expectedUpdate4 = new ParserResult();
+						
+		expectedUpdate4.setTaskId(12);
+		expectedUpdate4.setCommand(CommandType.UPDATE);
+		expectedUpdate4.setStartTime(new DateTime(2013,2,11,00,00));
+						
+		testExecuteStartTime(expectedUpdate4.getStartTime().toString(NIConstants.DISPLAY_DATE_FORMAT),"12,Start,feb 11 2013");
+		testExecuteCommandType(expectedUpdate4.getCommand().toString(),"12,Start,feb 11 2013");
+		testExecute(expectedUpdate4.getTaskID(), "12,Start,feb 11 2013");
+					
+		// Update start time field
+		ParserResult expectedUpdate5 = new ParserResult();
+								
+		expectedUpdate5.setTaskId(12);
+		expectedUpdate5.setCommand(CommandType.UPDATE);
+		expectedUpdate5.setEndTime(new DateTime(2013,2,11,00,00));
+								
+		testExecuteEndTime(expectedUpdate5.getEndTime().toString(NIConstants.DISPLAY_DATE_FORMAT),"12,end,feb 11 2013");
+		testExecuteCommandType(expectedUpdate5.getCommand().toString(),"12,end,feb 11 2013");
+		testExecute(expectedUpdate5.getTaskID(), "12,end,feb 11 2013");
+	
 		
 	}
 	
@@ -52,6 +99,14 @@ public class UpdateParserTest {
 		assertEquals(expected,testUpdate.execute().getEndTime().toString(NIConstants.DISPLAY_DATE_FORMAT));
 	}
 	
+	private void testExecuteCommandType (String expected, String command) throws InvalidCommandFormatException{
+		UpdateParser testUpdate = new UpdateParser(command);
+		assertEquals(expected,testUpdate.execute().getCommand().toString());
+	}
 	
+	private void testExecute (int expected, String command) throws InvalidCommandFormatException{
+		UpdateParser testUpdate = new UpdateParser(command);
+		assertEquals(expected,testUpdate.execute().getTaskID());
+	}
 
 }
