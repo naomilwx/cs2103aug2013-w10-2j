5f73a2670b83572a117e07c6ad045d84831b2978
Naomi Leow <naomileowwx@gmail.com>
2013-11-11 19:01:23 +0800

    merged with remote

---
+++ nailit/src/nailit/logic/parser/ParserExceptionConstants.java
@@ -0,0 +1,24 @@
+package nailit.logic.parser;
+
+public class ParserExceptionConstants {
+	public static final String EMPTY_COMMAND_TYPE = "Wrong Format: Your input string cannot be empty";
+	public static final String INVLID_COMMAND = "Wrong Format: This is an invalid command";
+	public static final String EMPTY_INPUT_STRING_ADD = "Wrong Format: Cannot add an empth task, please specify your task name";
+	public static final String EMPTY_INPUT_STRING_ADDREMINDER = "Wrong Format: Cannot add reminder an empth task, please specify your task name";
+	public static final String EMPTY_INPUT_STRING_COMPLETE = "Wrong Format: Cannot set an empth task to be completed, please specify your task name";
+	public static final String EMPTY_INPUT_STRING_DELETE = "Wrong Format: Cannot delete an empth task, please specify your task name";
+	public static final String EMPTY_INPUT_STRING_DELETEREMINDER = "Wrong Format: Cannot delete reminder an empth task, please specify your task name";
+	public static final String EMPTY_INPUT_STRING_DISPLAY = "Wrong Format: Cannot display an empth task, please specify your task name";
+	public static final String EMPTY_INPUT_STRING_SEARCH = "Wrong Format: Cannot search an empth task";
+	public static final String EMPTY_INPUT_STRING_UNCOMPLETE = "Wrong Format: Cannot set an empth task to be uncompleted, please specify the task ID";
+	public static final String EMPTY_INPUT_STRING_UPDATE = "Wrong Format: Wrong Format: Cannot update an empth task, please specify your task ID";
+	public static final String EMPTY_INPUT_STRING = "Wrong Format: Your input string cannot be empty";
+	public static final String RESERVE_WORD_CLASH = "Wrong Format: Your input string contains reserved string: \"$%\"";
+	public static final String BRACKET_UNMATCHED = "Wrong Format: Your brackets are not matched";
+	public static final String WRONG_TIME_FORMAT = "Wrong format: The string is not a correct time format";
+	public static final String WRONG_TAG_FORMAT = "Wrong format: The string cannot represent tag";
+	public static final String WRONG_PRIORITY_FORMAT = "Wrong format: The string cannot represent priority";
+	public static final String NO_NAME = "Wrong Format: Your name cannot be empty";
+	public static final String NO_TASK_ID = "Wrong Format: Your TaskID cannot be empty";
+	public static final String INVALID_STRING = "Wrong Format: The string cannot be identified.";
+}
