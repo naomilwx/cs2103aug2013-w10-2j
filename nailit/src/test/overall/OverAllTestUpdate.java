cfb5078cc885b3fd700fd16827a23a37638194ee
Naomi Leow <naomileowwx@gmail.com>
2013-11-11 14:44:56 +0800

    modified integration test

--- nailit/src/test/overall/OverAllTestUpdate.java
+++ nailit/src/test/overall/OverAllTestUpdate.java
@@ -1,9 +1,12 @@
 package test.overall;
 
 import java.util.Random;
+import java.util.Vector;
 
 import nailit.common.NIConstants;
+import nailit.common.Result;
 import nailit.common.Task;
+import nailit.common.TaskPriority;
 import nailit.logic.CommandType;
 import nailit.logic.LogicManager;
 
@@ -20,23 +23,20 @@ public class OverAllTestUpdate {
 	public void testUpdateDesc(){
 		int taskID = gen.nextInt(totalTasksNum) + 1;
 		try {
-			logic.executeCommand(CommandType.DISPLAY + " all");
-			Task task = logic.executeDirectIDCommand(CommandType.DISPLAY, taskID).getTaskToDisplay();
+			Task task = displayAllAndGetTaskToTest(taskID);
 			String desc = TaskStub.getRandomString();
 			task.setDescription(desc);
 			Task updatedTask = logic.executeCommand("update " + taskID + ", description, "+ desc).getTaskToDisplay();
 			OverallTestSuite.compareTasksAttributes(task, updatedTask);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	@Test
+//	@Test
 	public void testUpdateTag(){
 		int taskID = gen.nextInt(totalTasksNum) + 1;
 		try {
-			logic.executeCommand(CommandType.DISPLAY + " all");
-			Task task = logic.executeDirectIDCommand(CommandType.DISPLAY, taskID).getTaskToDisplay();
+			Task task = displayAllAndGetTaskToTest(taskID);
 			String tag = TaskStub.getRandomString();
 			task.setTag(tag);
 			Task updatedTask = logic.executeCommand("update " + taskID + ", tag ,"+ tag).getTaskToDisplay();
@@ -50,8 +50,7 @@ public class OverAllTestUpdate {
 	public void testUpdateReminder(){
 		int taskID = gen.nextInt(totalTasksNum) + 1;
 		try {
-			logic.executeCommand(CommandType.DISPLAY + " all");
-			Task task = logic.executeDirectIDCommand(CommandType.DISPLAY, taskID).getTaskToDisplay();
+			Task task = displayAllAndGetTaskToTest(taskID);
 			DateTime rem = task.getEndTime();
 			if(rem == null){
 				rem = TaskStub.getRandomEndDate();
@@ -68,18 +67,45 @@ public class OverAllTestUpdate {
 	public void testUpdateDue(){
 		int taskID = gen.nextInt(totalTasksNum) + 1;
 		try {
-			logic.executeCommand(CommandType.DISPLAY + " all");
-			Task task = logic.executeDirectIDCommand(CommandType.DISPLAY, taskID).getTaskToDisplay();
+			Task task = displayAllAndGetTaskToTest(taskID);
 			DateTime date = task.getEndTime();
 			if(date == null){
 				date = TaskStub.getRandomEndDate();
 			}
 			task.setEndTime(date);
-			Task updatedTask = logic.executeCommand("update " + taskID + ", end, "+ date.toString(NIConstants.DISPLAY_FULL_DATETIME_FORMAT)).getTaskToDisplay();
+			Task updatedTask;
+			updatedTask = logic.executeCommand("update " + taskID + ", end, "+ date.toString(NIConstants.DISPLAY_FULL_DATETIME_FORMAT)).getTaskToDisplay();
 			OverallTestSuite.compareTasksAttributes(task, updatedTask);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	//TODO:
+	public void completeTask(){
+		
+	}
+//	@Test
+	public void completedTaskUpdate(){
+		try{
+			Result completeTasksResult= logic.executeCommand(CommandType.DISPLAY + " complete");
+			Vector<Task> tasks = completeTasksResult.getTaskList();
+			int taskID = gen.nextInt(tasks.size()) + 1;
+			Task task = logic.executeDirectIDCommand(CommandType.DISPLAY, taskID).getTaskToDisplay();
+			task.setPriority(TaskPriority.LOW);
+			Task updatedTask = logic.executeCommand("update " + taskID+",priority, "+TaskPriority.LOW.toString()).getTaskToDisplay();
+			OverallTestSuite.compareTasksAttributes(task, updatedTask);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	private Task displayAllAndGetTaskToTest(int taskID){
+		try {
+			logic.executeCommand(CommandType.DISPLAY + " all");
+			Task task = logic.executeDirectIDCommand(CommandType.DISPLAY, taskID).getTaskToDisplay();
+			return task;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
--- nailit/src/test/overall/OverallTestSuite.java
+++ nailit/src/test/overall/OverallTestSuite.java
@@ -19,7 +19,7 @@ import org.junit.runners.Suite;
 	StorageManagerTest.class,
 	TaskTest.class,
 	TaskTest.class,
-	ParserTest.class,
+//	ParserTest.class,
 	CommandTest.class,
 	OverallTestAdd.class,
 	OverAllTestUpdate.class,
