cb30f0960e43ba81c0afc4797875dbf0073e0270
Tang Binbin tangbbnus <Tang Binbin tangbbnus@gmail.com>
2013-11-11 20:08:26 +0800

    add unit test for addparser

--- nailit/src/test/logic/parser/AddParserTest.java
+++ nailit/src/test/logic/parser/AddParserTest.java
@@ -21,14 +21,12 @@ public class AddParserTest {
 			ParserResult expectedAdd = new ParserResult();
 			ParserResult expectedAdd2 = new ParserResult();
 			
-			DateTime expectedStartDate;
-			DateTime expectedEndDate;
 			
 			expectedAdd.setName("CSAssignment");
 			expectedAdd.setCommand(CommandType.ADD);
 			expectedAdd.setPriority(TaskPriority.LOW);
-			expectedAdd.setStartTime(expectedStartDate = new DateTime(2013,9,11,00,00));
-			expectedAdd.setEndTime(expectedEndDate = new DateTime(2013,9,12,00,00));
+			expectedAdd.setStartTime(new DateTime(2013,9,11,00,00));
+			expectedAdd.setEndTime(new DateTime(2013,9,12,00,00));
 			expectedAdd.setTag("#study#");
 			
 			testExecuteName(expectedAdd.getName(),"CSAssignment,#study#, 11 Sep 2013, LOW");
@@ -38,12 +36,18 @@ public class AddParserTest {
 			testExecuteStartTime(expectedAdd.getStartTime().toString(NIConstants.DISPLAY_DATE_FORMAT),"CSAssignment,#study#, 11 Sep 2013, LOW");
 			
 			// this is the test for special cases(boundary case) for "from .. to .."
-			testExecuteName(expectedAdd.getName(),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
-			testExecuteCommandType(expectedAdd.getCommand().toString(),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
-			testExecuteTaskPriority(expectedAdd.getPriority().toString(),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
-			testExecuteTag(expectedAdd.getTag(),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 20133, LOW");
-			testExecuteStartTime(expectedAdd.getStartTime().toString(NIConstants.DISPLAY_DATE_FORMAT),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
-			testExecuteEndTime(expectedAdd.getEndTime().toString(NIConstants.DISPLAY_DATE_FORMAT),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
+			expectedAdd.setName("CSAssignment");
+			expectedAdd.setCommand(CommandType.ADD);
+			expectedAdd.setPriority(TaskPriority.LOW);
+			expectedAdd.setStartTime(expectedStartDate = new DateTime(2013,9,11,00,00));
+			expectedAdd.setEndTime(expectedEndDate = new DateTime(2013,9,12,00,00));
+			expectedAdd.setTag("#study#");
+			testExecuteName(expectedAdd2.getName(),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
+			testExecuteCommandType(expectedAdd2.getCommand().toString(),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
+			testExecuteTaskPriority(expectedAdd2.getPriority().toString(),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
+			testExecuteTag(expectedAdd2.getTag(),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 20133, LOW");
+			testExecuteStartTime(expectedAdd2.getStartTime().toString(NIConstants.DISPLAY_DATE_FORMAT),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
+			testExecuteEndTime(expectedAdd2.getEndTime().toString(NIConstants.DISPLAY_DATE_FORMAT),"CSAssignment,#study#, from 11 Sep 2013 to 12 Sep 2013, LOW");
 			
 		}
 		
